5826c4eedaa83136fa268709fbab5a8d86b1eef9
yeabtsega45 <yabtsega2022@gmail.com>
2024-01-08 20:49:41 +0300

    vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import svgr from "@svgr/rollup";
-import svgo from 'vite-plugin-svgo';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), svgo()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
