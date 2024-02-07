97f8bbf9aa25655922e4de69760377c5e50ae8a9
Yidne <yamiyekn@gmail.com>
2024-02-07 18:51:56 +0300

    Add navigation and log for the header while the scrollis below 620.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -187,7 +187,125 @@ const Header: React.FC = () => {
         )
         }
       </div>
-
+      {/* Header when the scroll is below 620  */}
+      <div className="relative z-10 py-2">
+        <div className="absolute top-[100%]  w-full  md:top-[15%] lg:top-[25%] xl:top-[80%]">
+          <div className=" flex justify-between items-center text-white font-nokia-bold w-[90%] md:w-[95%] lg:w-[85%] mx-auto">
+            {/* Logo */}
+            <div className="flex justify-center items-center  md:space-x-0   xl:space-x-1 cursor-pointer ">
+              <img src="src/assets/ezra-logo.svg" className="w-8 h-5 md:w-10 md:h-6 lg:w-10 lg:h-7 xl:w-12 xl:h-9" alt="" />
+              <NavLink to="/" onClick={closeMenu}>
+                <h3 className="text-xs md:text-sm lg:text-lg xl:text-2xl">
+                  <strong >Ezra</strong> Seminary
+                </h3>
+              </NavLink>
+            </div>
+            {/* Navigation and Logo */}
+            <nav>
+              {/* MenuBar */}
+              <div className="md:hidden  flex">
+                <button
+                  onClick={handleMenuClick}
+                  className="text-white focus:outline-none "
+                >
+                  {showMenu ? (
+                    <FaTimes
+                      size={20}
+                      className="z-20 fixed top-[2.2rem] left-[90%]"
+                    />
+                  ) : (
+                    <FaBars size={20} className=" z-20 top-[2.7rem] left-[90%] block" />
+                  )}
+                </button>
+              </div>
+              {/* Navigation */}
+              <ul
+                className={`${showMenu
+                  ? "flex flex-col justify-center items-end text-xl font-nokia-bold h-auto bg-secondary-6 overflow-auto bg-opacity-80  w-full z-10 top-0 left-0 bottom-0 transform -translate-x-100 transition-transform ease-in-out duration-200 pr-8 space-y-2 fixed cursor-pointer"
+                  : "hidden font-Lato-Regular  cursor-pointer  md:flex md:items-center md:justify-end md:text-xs gap-[0.4rem] lg:space-x-2 xl:text-lg xl:space-x-2  "
+                  }`}
+              >
+                <li className="hover:text-accent-6">
+                  <NavLink to="/" onClick={closeMenu}>
+                    Home
+                  </NavLink>
+                </li>
+                <li className="hover:text-accent-6">
+                  <NavLink to="/courses" onClick={closeMenu}>
+                    Courses
+                  </NavLink>
+                </li>
+                <li className="hover:text-accent-6">
+                  <NavLink to="/sabbathSchool" onClick={closeMenu}>
+                    Sabbath School
+                  </NavLink>
+                </li>
+                <li className="hover:text-accent-6">
+                  <NavLink to="/devotion" onClick={closeMenu}>
+                    Devotion
+                  </NavLink>
+                </li>
+                <li className="hover:text-accent-6">
+                  <NavLink to="/aboutUs" onClick={closeMenu}>
+                    About Us
+                  </NavLink>
+                </li>
+                <li className="hover:text-accent-6">
+                  <NavLink to="/contactUs" onClick={closeMenu}>
+                    Contact Us
+                  </NavLink>
+                </li>
+                {user ? (
+                  <li ref={ref} className="relative">
+                    <div
+                      className="flex items-center space-x-2 bg-accent-6 rounded-full px-2 py-1 text-xs1  hover:bg-accent-7 cursor-pointer"
+                      onClick={handleAccountClick}
+                    >
+                      <FaRegUserCircle />
+                      <div className="text-xs  xl:text-lg font-medium text-white">
+                        {user.firstName}
+                        <div className="text-xs  xl:text-lg text-white-400">{user.role}</div>
+                      </div>
+                    </div>
+                    {showAccountModal && (
+                      <div className="absolute top-[40px] right-0 bg-accent-6 shadow-lg rounded-md z-10">
+                        <div className="px-4 py-2 border-b">
+                          Logged in as: {user.email}
+                        </div>
+                        <div className="px-4 py-2 border-b">
+                          Role: {user.role}
+                        </div>
+                        <div className="px-4 py-2 border-b">
+                          <NavLink to="/profile">Profile Settings</NavLink>
+                        </div>
+                        {user.role === "Admin" && (
+                          <div className="px-4 py-2 border-b">
+                            <NavLink to="/admin">Dashboard</NavLink>
+                          </div>
+                        )}
+                        <div className="px-4 py-2">
+                          <LogoutButton />
+                        </div>
+                      </div>
+                    )}
+                  </li>
+                ) : (
+                  <>
+                    <li className="hover:text-gray-400">
+                      <NavLink to="/logIn">Log In</NavLink>
+                    </li>
+                    <li className="hover:text-gray-400 text-base ">
+                      <NavLink to="/signup">
+                        <Button size="round" className=" bg-accent-6 rounded-full px-3 py-1 text-xs xl:px-5 xl:py-7  xl:text-lg  hover:bg-accent-7 cursor-pointer transition-all">Create Account</Button>
+                      </NavLink>
+                    </li>
+                  </>
+                )}
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
