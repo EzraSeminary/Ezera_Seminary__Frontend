87c20ecf6d0c2dc77bad910d78bbc66c9aecbe47
Yidne <yamiyekn@gmail.com>
2024-02-07 19:36:49 +0300

    Add and Styling the testimonials contents.

--- src/features/home/Testimonials.tsx
+++ src/features/home/Testimonials.tsx
@@ -1,4 +1,6 @@
-
+import melak from "../../assets/melak.svg";
+import janet from "../../assets/janet.svg";
+import dawit from "../../assets/dawit.webp";
 
 const Testimonials = () => {
   return (
@@ -12,7 +14,53 @@ const Testimonials = () => {
         </h1>
         <hr className="border-accent-5 border-1 w-[90%] pb-3 mx-auto" />
 
+        {/*Container for the Testimonials contents */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-6">
 
+          {/* Testimonial 1 */}
+          <div className="flex flex-col w-full mt-3">
+            <div className="mx-auto  w-[40%] ">
+              <img className="rounded-full border p-1 border-accent-6" src={melak} alt="melak" />
+            </div>
+            <div className="text-center py-4 font-nokia-bold">
+              <h2 className="text-accent-5 pb-2 text-lg lg:text-xl xl:text-2xl">
+                ፓ/ር መልዓክ አለማየሁ (ዶ/ር)
+              </h2>
+              <p className="text-secondary-6 font-nokia-light text-sm xl:text-lg mb-4">
+                “መጽሃፍ ቅዱስን ማንበብ ከምንም በላይ ለህይወታችን አስፈላጊው እና ዋነኛው ነገር ነው።መጽሃፍ ቅዱስን
+                ማንበብ ከምንም በላይ ለህይወታችን አስፈላጊው እና ዋነኛው ነገር ነው።”
+              </p>
+            </div>
+          </div>
+
+          {/* Testimonial 2 */}
+          <div className="flex flex-col w-full mt-3">
+            <div className="mx-auto w-[40%]">
+              <img className="rounded-full border p-1 border-accent-6" src={janet} alt="janet" />
+            </div>
+            <div className="text-center py-4 font-nokia-bold">
+              <h2 className="text-accent-5 pb-2 text-lg lg:text-xl xl:text-2xl">ውብገነት ቦጋለ (ጃኔት)</h2>
+              <p className="text-secondary-6 font-nokia-light text-sm xl:text-lg mb-4">
+                “መጽሃፍ ቅዱስን ማንበብ ከምንም በላይ ለህይወታችን አስፈላጊው እና ዋነኛው ነገር ነው።መጽሃፍ ቅዱስን
+                ማንበብ ከምንም በላይ ለህይወታችን አስፈላጊው እና ዋነኛው ነገር ነው።”
+              </p>
+            </div>
+          </div>
+
+          {/* Testimonial 3 */}
+          <div className="flex flex-col w-full mt-3">
+            <div className="mx-auto w-[40%]">
+              <img className="rounded-full border p-1 border-accent-6" src={dawit} alt="dawit" />
+            </div>
+            <div className="text-center py-4 font-nokia-bold">
+              <h2 className="text-accent-5 pb-2 text-lg lg:text-xl xl:text-2xl">ዳዊት መሃሪ</h2>
+              <p className="text-secondary-6 font-nokia-light text-sm xl:text-lg mb-4">
+                “መጽሃፍ ቅዱስን ማንበብ ከምንም በላይ ለህይወታችን አስፈላጊው እና ዋነኛው ነገር ነው።መጽሃፍ ቅዱስን
+                ማንበብ ከምንም በላይ ለህይወታችን አስፈላጊው እና ዋነኛው ነገር ነው።”
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
