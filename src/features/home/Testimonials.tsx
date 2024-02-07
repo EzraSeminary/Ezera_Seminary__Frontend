ac50250121693b7dc1b77d1759c9905cf891e4d2
Yidne <yamiyekn@gmail.com>
2024-02-07 19:16:18 +0300

    Add the Testimonials  component.

--- src/features/home/Testimonials.tsx
+++ src/features/home/Testimonials.tsx
@@ -5,45 +5,45 @@ import dawit from "../../assets/dawit.webp";
 const Testimonials = () => {
   return (
     <div className="">
-      <div className="flex-1 border-2 border-yellow-800 flex flex-col items-center justify-center space-y-4 w-[100%] mx-auto">
-        <h1 className="text-3xl font-nokia-bold  text-secondary-6">
+      <div className="flex-1 container mx-auto px-4 space-y-4">
+        <h1 className="text-3xl font-nokia-bold  text-secondary-6 text-center ">
           Testimonials
         </h1>
-        <hr className="border-accent-5 border-1 w-[100%] pb-3" />
-        <div className="flex flex-col w-[80%] justify-center items-center  mx-auto space-y-6 md:flex-row md:space-x-4 md:space-y-0 ">
-          <div className="flex  flex-col w-[33.%] md:w-[33.3%] mt-6">
-            <div className="mx-auto w-[33.%]">
-              <img className="rounded-full" src={melak} alt="melak" />
+        <hr className="border-accent-5 border-1 w-[90%] pb-3 mx-auto" />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-6">
+          <div className="flex flex-col w-full mt-3">
+            <div className="mx-auto  w-[40%] ">
+              <img className="rounded-full border p-1 border-accent-6" src={melak} alt="melak" />
             </div>
             <div className="text-center py-4 font-nokia-bold">
-              <h2 className="text-accent-5 text-2xl pb-2">
+              <h2 className="text-accent-5 pb-2 text-lg lg:text-xl xl:text-2xl">
                 ፓ/ር መልዓክ አለማየሁ (ዶ/ር)
               </h2>
-              <p className="text-secondary-6 font-nokia-light text-lg mb-4">
+              <p className="text-secondary-6 font-nokia-light text-sm xl:text-lg mb-4">
                 “መጽሃፍ ቅዱስን ማንበብ ከምንም በላይ ለህይወታችን አስፈላጊው እና ዋነኛው ነገር ነው።መጽሃፍ ቅዱስን
                 ማንበብ ከምንም በላይ ለህይወታችን አስፈላጊው እና ዋነኛው ነገር ነው።”
               </p>
             </div>
           </div>
-          <div className="flex  flex-col w-[33.%] md:w-[33.3%]">
-            <div className="mx-auto w-[33.%]">
-              <img className="rounded-full" src={janet} alt="janet" />
+          <div className="flex flex-col w-full mt-3">
+            <div className="mx-auto w-[40%]">
+              <img className="rounded-full border p-1 border-accent-6" src={janet} alt="janet" />
             </div>
             <div className="text-center py-4 font-nokia-bold">
-              <h2 className="text-accent-5 text-2xl pb-2">ውብገነት ቦጋለ (ጃኔት)</h2>
-              <p className="text-secondary-6 font-nokia-light text-lg mb-4">
+              <h2 className="text-accent-5 pb-2 text-lg lg:text-xl xl:text-2xl">ውብገነት ቦጋለ (ጃኔት)</h2>
+              <p className="text-secondary-6 font-nokia-light text-sm xl:text-lg mb-4">
                 “መጽሃፍ ቅዱስን ማንበብ ከምንም በላይ ለህይወታችን አስፈላጊው እና ዋነኛው ነገር ነው።መጽሃፍ ቅዱስን
                 ማንበብ ከምንም በላይ ለህይወታችን አስፈላጊው እና ዋነኛው ነገር ነው።”
               </p>
             </div>
           </div>
-          <div className="flex  flex-col w-[33.%] md:w-[33.3%]">
-            <div className="mx-auto w-[33.%]">
-              <img className="rounded-full" src={dawit} alt="dawit" />
+          <div className="flex flex-col w-full mt-3">
+            <div className="mx-auto w-[40%]">
+              <img className="rounded-full border p-1 border-accent-6" src={dawit} alt="dawit" />
             </div>
             <div className="text-center py-4 font-nokia-bold">
-              <h2 className="text-accent-5 text-2xl pb-2">ዳዊት መሃሪ</h2>
-              <p className="text-secondary-6 font-nokia-light text-lg mb-4">
+              <h2 className="text-accent-5 pb-2 text-lg lg:text-xl xl:text-2xl">ዳዊት መሃሪ</h2>
+              <p className="text-secondary-6 font-nokia-light text-sm xl:text-lg mb-4">
                 “መጽሃፍ ቅዱስን ማንበብ ከምንም በላይ ለህይወታችን አስፈላጊው እና ዋነኛው ነገር ነው።መጽሃፍ ቅዱስን
                 ማንበብ ከምንም በላይ ለህይወታችን አስፈላጊው እና ዋነኛው ነገር ነው።”
               </p>
