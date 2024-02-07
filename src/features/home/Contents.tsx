b0b133c6b567910be0bc86579ab6b64246729865
Yidne <yamiyekn@gmail.com>
2024-02-07 19:36:07 +0300

    Styling the testimonial component.

--- src/features/home/Contents.tsx
+++ src/features/home/Contents.tsx
@@ -25,21 +25,27 @@ const Contents = () => {
 
   return (
     <div className="w-full max-w-7xl mx-auto px-4">
+
       {/* Contents section */}
       <div className=" flex flex-col items-center justify-center space-y-4 ">
+
         {/* Title of the page */}
         <h1 className="text-3xl font-nokia-bold text-secondary-6 text-center">
           Our Contents
         </h1>
         <hr className="border-accent-5 border-1 w-[90%] pb-3" />
+
         {/* container of the Contents */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 ">
+
           {/* Content 1 */}
           <div className="bg-secondary-6 bg-opacity-90 text-center pb-4 font-nokia-bold rounded-xl shadow-2xl">
+
             {/* Image of the content */}
             <div className="mx-auto w-[100%]">
               <img className="w-full" src={content1} alt="content1" />
             </div>
+
             {/* Texts and button of the content */}
             <h2 className="text-accent-5 text-lg lg:text-xl xl:text-2xl mt-4">Course Study</h2>
             <p className="text-[#fff] font-nokia-bold text-sm xl:text-lg mb-4">
@@ -53,12 +59,15 @@ const Contents = () => {
               See More
             </button>
           </div>
+
           {/* Content 2 */}
           <div className="bg-secondary-6 bg-opacity-90 text-center pb-4 font-nokia-bold rounded-xl shadow-2xl">
+
             {/* Image of the content */}
             <div className="mx-auto w-[100%]">
               <img className="w-full" src={content2} alt="content2" />
             </div>
+
             {/* Texts and button of the content */}
             <h2 className="text-accent-5 text-lg lg:text-xl xl:text-2xl mt-4">Sabbath School Study</h2>
             <p className="text-[#fff] font-nokia-bold text-sm xl:text-lg mb-4">
@@ -74,10 +83,12 @@ const Contents = () => {
           </div>
 
           <div className="bg-secondary-6 bg-opacity-90 text-center pb-4 font-nokia-bold rounded-xl shadow-2xl">
+
             {/* Image of the content */}
             <div className="mx-auto w-[100%]">
               <img className="w-full" src={content3} alt="content3" />
             </div>
+
             {/* Texts and button of the content */}
             <h2 className="text-accent-5 text-lg lg:text-xl xl:text-2xl mt-4">Devotional Study</h2>
             <p className="text-[#fff] font-nokia-bold text-sm xl:text-lg mb-4">
