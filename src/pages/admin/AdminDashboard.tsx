ea97a6f48e9a597f99398e4c4b6a6234e82ecb60
yeabtsega45 <yabtsega2022@gmail.com>
2024-02-08 13:36:50 +0300

    working on AdminChapter

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -2,11 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import Sidebar from "../../components/Sidebar";
 import ManageCourse from "./ManageCourse";
 import CreateCourse from "./CreateCourse";
+import AdminChapter from "../../features/courses/admin/AdminChapter";
 // import SabbathSchool from "../routes/SabbathSchool";
 // import CreateDevotion from "./CreateDevotion";
 // import ManageDevotion from "./ManageDevotion";
 // import Devotion from "../routes/Devotion";
-// import AdminChapter from "../features/CourseComponents/AdminChapter";
 // import EditCourse from "../components/admin/EditCourse";
 // import EditCourseFirst from "@/components/admin/EditCourseFirst";
 
@@ -19,11 +19,11 @@ const AdminDashboard = () => {
           <Route path="/" element={<div>Admin Home</div>} />
           <Route path="course/edit" element={<ManageCourse />} />
           <Route path="courses/create" element={<CreateCourse />} />
+          <Route path="courses/create/chapters" element={<AdminChapter />} />
           {/* <Route path="sabbathSchool" element={<SabbathSchool />} />
           <Route path="devotion" element={<Devotion />} />
           <Route path="devotion/create" element={<CreateDevotion />} />
           <Route path="devotion/manage" element={<ManageDevotion />} />
-          <Route path="courses/create/chapters" element={<AdminChapter />} />
           <Route path="edit/course/:id" element={<EditCourseFirst />} />
           <Route path="edit/course/:id/chapters" element={<EditCourse />} /> */}
         </Routes>
