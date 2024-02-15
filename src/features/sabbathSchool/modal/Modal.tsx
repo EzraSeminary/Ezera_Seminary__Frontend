5c7c71b7cc1b22011a111d8e4956b11db14659a4
AmanWorku <amexworku2014@gmail.com>
2024-02-15 12:57:03 +0300

    Fix modal styling

--- src/features/sabbathSchool/modal/Modal.tsx
+++ src/features/sabbathSchool/modal/Modal.tsx
@@ -1,25 +1,29 @@
 import React from "react";
 import "./ModalStyles.css";
+import { X } from "@phosphor-icons/react";
 const Modal = ({ showModal, toggleModal, children }: { showModal: boolean, toggleModal: () => void, children: React.ReactNode }) => {
+  if (!showModal) return null;
+
   return (
-   showModal && 
-   ( <div className="fixed inset-0 z-40 overflow-y-auto h-50%">
-      <div className="fixed inset-0 bg-gray-500 bg-opacity-50"></div>
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="modal-content z-50 bg-white rounded-lg shadow-xl transform transition-all sm:max-w-lg sm:w-full">
-          <div className="p-6 text-justify">
-            <span
-            className="close absolute top-0 right-0 m-4 text-2xl cursor-pointer"
-            onClick={toggleModal}
+    <div className="fixed inset-0 z-40 flex items-center justify-center">
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-50" onClick={toggleModal} />
+      <div className="relative px-4 w-full max-w-2xl h-full md:h-auto">
+        <div className="modal-content relative bg-white rounded-lg shadow overflow-y-auto" 
+             style={{ maxHeight: '50vh' }}>
+          <div className="flex justify-end p-2">
+            <X 
+              className="p-1 text-xl bg-accent-6 text-primary-1 rounded-full hover:bg-accent-8 hover:cursor-pointer transition-all"
+              size={24}
+              onClick={toggleModal}
             >
-            ×
-            </span>
-          {children}
+            </X>
+          </div>
+          <div className="px-6 py-2">
+            {children}
           </div>
         </div>
       </div>
-    </div>)
-  
+    </div>
   );
 }
 
