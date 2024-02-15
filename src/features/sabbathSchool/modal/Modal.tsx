db35f486dcd5ac2a0fe6d14f977dc1cec7af8c9a
AmanWorku <amexworku2014@gmail.com>
2024-02-15 12:27:10 +0300

    Fix modal styling

--- src/features/sabbathSchool/modal/Modal.tsx
+++ src/features/sabbathSchool/modal/Modal.tsx
@@ -1,21 +1,26 @@
-// ModalComponent.tsx
-
 import React from "react";
-import './ModalStyles.css'
+import "./ModalStyles.css";
 const Modal = ({ showModal, toggleModal, children }: { showModal: boolean, toggleModal: () => void, children: React.ReactNode }) => {
-    return (
-        showModal && (
-          <div className="modal">
-            <div className="modal-content">
-              <span className="close" onClick={toggleModal}>
-                ×
-              </span>
-              {children}
-            </div>
+  return (
+   showModal && 
+   ( <div className="fixed inset-0 z-40 overflow-y-auto h-50%">
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-50"></div>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="modal-content z-50 bg-white rounded-lg shadow-xl transform transition-all sm:max-w-lg sm:w-full">
+          <div className="p-6 text-justify">
+            <span
+            className="close absolute top-0 right-0 m-4 text-2xl cursor-pointer"
+            onClick={toggleModal}
+            >
+            ×
+            </span>
+          {children}
           </div>
-        )
-      );
-        }
+        </div>
+      </div>
+    </div>)
+  
+  );
+}
 
 export default Modal;
-
