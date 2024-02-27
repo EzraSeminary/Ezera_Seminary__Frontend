109cbc8121a1955e8963da28e1380b85bd45690e
Besufekad-HAZ <besurye@gmail.com>
2024-02-27 17:37:34 +0300

    Change onrender links to the newly deployed link

--- src/features/courses/user/ChaptersDisplay.tsx
+++ src/features/courses/user/ChaptersDisplay.tsx
@@ -3,24 +3,36 @@ import { NavLink, useParams } from "react-router-dom";
 import { useGetCourseByIdQuery } from "../../../services/api";
 import { useOnClickOutside } from "../../../hooks/useOnClickOutside";
 import BeatLoader from "react-spinners/BeatLoader";
-import { ArrowLeft, CheckCircle, Circle, XCircle, ArrowRight } from "@phosphor-icons/react";
+import {
+  ArrowLeft,
+  CheckCircle,
+  Circle,
+  XCircle,
+  ArrowRight,
+} from "@phosphor-icons/react";
 import logo from "../../../assets/ezra-logo.svg";
 import bibleImage from "../../../assets/bible2.jpeg";
 
 function ChaptersDisplay() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [open, setOpen] = useState<boolean>(true);
-  {/* State to control account modal  */ }
+  {
+    /* State to control account modal  */
+  }
 
   const [activeIndex, setActiveIndex] = useState<number>(0);
   const [unlockedIndex, setUnlockedIndex] = useState<number>(0); // New state variable to track the unlocked index
 
-  {/* Function to open the chapters sidebar modal */ }
+  {
+    /* Function to open the chapters sidebar modal */
+  }
   const handleArrowClick = () => {
     setOpen((prev) => !prev);
   };
 
-  {/* Ref to listen the curser and close the chapters sidebar modal */ }
+  {
+    /* Ref to listen the curser and close the chapters sidebar modal */
+  }
   const ref = useRef<HTMLDivElement>(null);
   useOnClickOutside(ref, open, () => setOpen(false));
 
@@ -58,7 +70,9 @@ function ChaptersDisplay() {
     return index <= unlockedIndex; // Check if the slide is unlocked based on the unlocked index
   };
 
-  {/* Loading state */ }
+  {
+    /* Loading state */
+  }
   if (isLoading)
     return (
       <div className="h-screen flex justify-center items-center">
@@ -85,19 +99,34 @@ function ChaptersDisplay() {
             to={"/courses"}
             className="flex items-center justify-between border-accent-5 border w-max rounded-3xl px-3 py-1 gap-2 hover:bg-[#FAE5C7]"
           >
-            <ArrowLeft size={22} className="text-white bg-accent-6 border p-1 rounded-lg" />
-            <button className="text-accent-6 text-xs font-nokia-bold">ተመለስ</button>
+            <ArrowLeft
+              size={22}
+              className="text-white bg-accent-6 border p-1 rounded-lg"
+            />
+            <button className="text-accent-6 text-xs font-nokia-bold">
+              ተመለስ
+            </button>
           </NavLink>
         </div>
 
         {/* Chapters side bar*/}
-        <div ref={ref} className={`absolute left-0 top-[10%]  lg:left-[4%] lg:top-[10%] flex flex-col justify-start items-center ${open ? "w-[80%] md:w-[50%] lg:w-[30%] z-40 h-[80%]" : "w-0 h-0"}`}
-          style={{ transition: "width 0.3s" }}>
-
+        <div
+          ref={ref}
+          className={`absolute left-0 top-[10%]  lg:left-[4%] lg:top-[10%] flex flex-col justify-start items-center ${
+            open ? "w-[80%] md:w-[50%] lg:w-[30%] z-40 h-[80%]" : "w-0 h-0"
+          }`}
+          style={{ transition: "width 0.3s" }}
+        >
           {open ? (
-            <ArrowLeft onClick={handleArrowClick} className="text-white text-2xl xl:text-3xl bg-accent-6 border p-1 rounded-full absolute -right-2 md:-right-3 lg:-right-2 xl:-right-3 top-14 cursor-pointer" />
+            <ArrowLeft
+              onClick={handleArrowClick}
+              className="text-white text-2xl xl:text-3xl bg-accent-6 border p-1 rounded-full absolute -right-2 md:-right-3 lg:-right-2 xl:-right-3 top-14 cursor-pointer"
+            />
           ) : (
-            <ArrowRight onClick={handleArrowClick} className="text-white text-2xl xl:text-3xl  bg-accent-6 border p-1 rounded-full absolute -right-3 xl:-right-4 top-14 cursor-pointer" />
+            <ArrowRight
+              onClick={handleArrowClick}
+              className="text-white text-2xl xl:text-3xl  bg-accent-6 border p-1 rounded-full absolute -right-3 xl:-right-4 top-14 cursor-pointer"
+            />
           )}
 
           {/* Bible image container*/}
@@ -105,18 +134,26 @@ function ChaptersDisplay() {
             <img
               src={bibleImage}
               // src={
-              //   `https://ezra-seminary-api.onrender.com/images/` +
+              //   `http://ezra-seminary.mybese.tech/images/` +
               //   courseData?.image
               // }
-              alt="Bible image" className="w-full rounded-t-lg" />
+              alt="Bible image"
+              className="w-full rounded-t-lg"
+            />
           </div>
 
           {/* Short information*/}
-          <div className={`  pl-2 py-1 bg-primary-7  gap-2 justify-between items-center  w-full text-xs1  lg:text-xs ${open ? "flex" : "hidden"}`}>
+          <div
+            className={`  pl-2 py-1 bg-primary-7  gap-2 justify-between items-center  w-full text-xs1  lg:text-xs ${
+              open ? "flex" : "hidden"
+            }`}
+          >
             <div className="p-1 bg-accent-6 rounded">
               <p className="font-Lato-Bold text-primary-1 ">10%</p>
             </div>
-            <p className="font-Lato-Bold text-secondary-6 leading-none">Pass 100% of your lessons to complete this course</p>
+            <p className="font-Lato-Bold text-secondary-6 leading-none">
+              Pass 100% of your lessons to complete this course
+            </p>
           </div>
 
           {/* Course title and description*/}
@@ -144,16 +181,16 @@ function ChaptersDisplay() {
                 return (
                   <button
                     key={index}
-                    className={`flex justify-between items-center font-nokia-bold border-b border-accent-5 px-2 text-secondary-6 cursor-pointer py-2 rounded-lg bg-gray-200 hover:bg-[#FAE5C7] hover:opacity-80  ${unlocked ? "text-black  " : "text-gray-500 "
-                      }  ${index === activeIndex && "font-bold"}
+                    className={`flex justify-between items-center font-nokia-bold border-b border-accent-5 px-2 text-secondary-6 cursor-pointer py-2 rounded-lg bg-gray-200 hover:bg-[#FAE5C7] hover:opacity-80  ${
+                      unlocked ? "text-black  " : "text-gray-500 "
+                    }  ${index === activeIndex && "font-bold"}
                     `} // Locked slide to gray
                     onClick={() => {
                       updateIndex(index);
                     }}
                   >
                     <div className="flex flex-col items-start justify-center">
-                      <h2
-                        className="font-nokia-bold text-secondary-6 text-xs lg:text-sm">
+                      <h2 className="font-nokia-bold text-secondary-6 text-xs lg:text-sm">
                         {chapter.chapter}
                         {/* <Text>ID</Text> {courseId} */}
                       </h2>
@@ -162,9 +199,9 @@ function ChaptersDisplay() {
                       </p>
                     </div>
                     {unlocked ? (
-                      <CheckCircle size={16} weight="fill" color={'#EA9215'} />
+                      <CheckCircle size={16} weight="fill" color={"#EA9215"} />
                     ) : (
-                      <Circle size={16} color={'#EA9215'} />
+                      <Circle size={16} color={"#EA9215"} />
                     )}
                   </button>
                 );
@@ -175,16 +212,17 @@ function ChaptersDisplay() {
 
         {/* Chapter display window*/}
         <div className=" lg:w-[92%] justify-start items-center mx-auto h-[80%] chapter-img-1 bg-no-repeat bg-cover bg-center rounded-lg ">
-
           {/* Chapter display container */}
           <div className="flex flex-col justify-between h-full">
-
             {/* Header */}
             <div>
               <div className="w-[90%] pt-4 pb-2 flex justify-between mx-auto items-center">
                 <div className=" z-30 h-full flex justify-center items-center  md:space-x-0   xl:space-x-1 cursor-pointer ">
-
-                  <img src={logo} className="w-8 h-5 md:w-10 md:h-6  z-30" alt="" />
+                  <img
+                    src={logo}
+                    className="w-8 h-5 md:w-10 md:h-6  z-30"
+                    alt=""
+                  />
 
                   <h3 className="text-white font-nokia-bold text-xs md:text-sm ">
                     <strong>Ezra</strong> Seminary
@@ -194,7 +232,11 @@ function ChaptersDisplay() {
                   to={"/courses"}
                   className="flex items-center justify-between border-accent-5 border w-max rounded-3xl px-3 py-1 gap-2 hover:bg-[#FAE5C7]"
                 > */}
-                <XCircle size={24} color={'#EA9215'} className="z-20 cursor-pointer" />
+                <XCircle
+                  size={24}
+                  color={"#EA9215"}
+                  className="z-20 cursor-pointer"
+                />
                 {/* </NavLink> */}
               </div>
               <hr className="border-accent-5 border-1 w-[90%] mx-auto" />
@@ -231,7 +273,7 @@ function ChaptersDisplay() {
             </div>
           </div>
         </div>
-      </div >
+      </div>
     </>
   );
 }
