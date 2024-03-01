0ea171fc097342c7fa7e7e62421361352f97e909
Besufekad-HAZ <besurye@gmail.com>
2024-03-01 17:55:54 +0300

    Add delete devotion confirmation pop-up windows

--- src/features/devotions/CurrentDevotional.tsx
+++ src/features/devotions/CurrentDevotional.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PropTypes from "prop-types";
 import type { AppDispatch } from "@/redux/store"; // Import the AppDispatch type
@@ -9,6 +10,7 @@ import {
 } from "../../redux/devotionsSlice";
 import { useGetDevotionsQuery } from "../../redux/api-slices/apiSlice";
 import { RootState, Devotion } from "@/redux/types";
+import Modal from "react-modal";
 
 interface CurrentDevotionalProps {
   devotionToDisplay: Devotion;
@@ -22,11 +24,13 @@ const CurrentDevotional: React.FC<CurrentDevotionalProps> = ({
   const { refetch } = useGetDevotionsQuery(); // get the authentication token
   const role = useSelector((state: RootState) => state.auth.user?.role); // get the authentication token
   const dispatch = useDispatch<AppDispatch>();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [devotionToDelete, setDevotionToDelete] = useState<string | null>(null); // new state variable
 
   const handleDelete = async (id: string) => {
-    // ❗❗ big errors to fix later
     await dispatch(deleteDevotion(id)); // dispatch delete action
     refetch(); // refetch the devotions data
+    setModalIsOpen(false); // close the modal
   };
 
   const startEditing = (devotion: Devotion) => {
@@ -43,6 +47,71 @@ const CurrentDevotional: React.FC<CurrentDevotionalProps> = ({
       </div>
       <div className="flex space-x-12">
         {/* Replace latestDevotion with devotionToDisplay */}
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={() => setModalIsOpen(false)}
+          contentLabel="Delete Confirmation"
+          style={{
+            overlay: {
+              backgroundColor: "rgba(0, 0, 0, 0.75)", // This will give the overlay a black color with 75% opacity
+            },
+            content: {
+              top: "50%",
+              left: "55%",
+              right: "auto",
+              bottom: "auto",
+              marginRight: "-50%",
+              transform: "translate(-50%, -50%)",
+              backgroundColor: "#fff", // This will give the modal a white background
+              border: "1px solid #ccc", // This will give the modal a border
+              borderRadius: "10px", // This will round the corners of the modal
+              padding: "20px", // This will add some padding inside the modal
+              width: "30%", // This will set the width of the modal to 30% of the viewport width
+              height: "30%", // This will set the height of the modal to 30% of the viewport height
+              display: "flex", // This will make the content a flex container
+              flexDirection: "column", // This will make the flex items stack vertically
+              justifyContent: "center", // This will center the flex items vertically
+              alignItems: "center", // This will center the flex items horizontally
+            },
+          }}
+        >
+          <h2 className="text-2xl font-Lato-black text-secondary-6 mb-2">
+            Are you sure you want to delete this devotion?
+          </h2>
+          <div>
+            <button
+              onClick={() => {
+                if (devotionToDelete) {
+                  handleDelete(devotionToDelete); // delete the devotion when "Yes" is clicked
+                }
+              }}
+              style={{
+                backgroundColor: "#ff0000", // red background
+                color: "#ffffff", // white text
+                border: "none", // remove border
+                borderRadius: "5px", // rounded corners
+                padding: "10px 20px", // vertical and horizontal padding
+                marginRight: "10px", // space between the buttons
+                cursor: "pointer", // change cursor on hover
+              }}
+            >
+              Yes
+            </button>
+            <button
+              onClick={() => setModalIsOpen(false)}
+              style={{
+                backgroundColor: "#1A2024", // green background
+                color: "#ffffff", // white text
+                border: "none", // remove border
+                borderRadius: "5px", // rounded corners
+                padding: "10px 20px", // vertical and horizontal padding
+                cursor: "pointer", // change cursor on hover
+              }}
+            >
+              No
+            </button>
+          </div>
+        </Modal>
         {devotionToDisplay &&
         (devotionToDisplay.month !== "" || devotionToDisplay.day !== "") ? (
           <div className="rounded-xl w-[20%] h-full border-2 bg-[#fff] border-accent-5 mt-8 text-secondary-6">
@@ -77,7 +146,10 @@ const CurrentDevotional: React.FC<CurrentDevotionalProps> = ({
               <>
                 <FaTrash
                   className="text-gray-700 text-xl cursor-pointer self-center"
-                  onClick={() => handleDelete(devotionToDisplay?._id || "")}
+                  onClick={() => {
+                    setDevotionToDelete(devotionToDisplay?._id || ""); // set the id to delete
+                    setModalIsOpen(true); // open the modal
+                  }}
                 />
                 <FaEdit
                   className="text-gray-700 text-xl cursor-pointer self-center "
