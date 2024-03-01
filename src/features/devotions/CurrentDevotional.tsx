0bb23dbe01f0338731557aa551ca82e0e47a4c2b
Besufekad-HAZ <besurye@gmail.com>
2024-03-01 18:10:42 +0300

    Setup toas in the current devotional to Implement toastify

--- src/features/devotions/CurrentDevotional.tsx
+++ src/features/devotions/CurrentDevotional.tsx
@@ -11,6 +11,8 @@ import {
 import { useGetDevotionsQuery } from "../../redux/api-slices/apiSlice";
 import { RootState, Devotion } from "@/redux/types";
 import Modal from "react-modal";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface CurrentDevotionalProps {
   devotionToDisplay: Devotion;
@@ -28,7 +30,9 @@ const CurrentDevotional: React.FC<CurrentDevotionalProps> = ({
   const [devotionToDelete, setDevotionToDelete] = useState<string | null>(null); // new state variable
 
   const handleDelete = async (id: string) => {
+    // One typescript error below to fix ❗❗❗
     await dispatch(deleteDevotion(id)); // dispatch delete action
+    toast.success("Devotion deleted successfully!");
     refetch(); // refetch the devotions data
     setModalIsOpen(false); // close the modal
   };
@@ -39,187 +43,189 @@ const CurrentDevotional: React.FC<CurrentDevotionalProps> = ({
   };
 
   return (
-    <div className="h-auto border-2 shadow-lg rounded-2xl p-6">
-      <div>
-        <h1 className="font-nokia-bold  text-3xl text-accent-5 ">
-          Daily Devotional
-        </h1>
-      </div>
-      <div className="flex space-x-12">
-        {/* Replace latestDevotion with devotionToDisplay */}
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={() => setModalIsOpen(false)}
-          contentLabel="Delete Confirmation"
-          style={{
-            overlay: {
-              backgroundColor: "rgba(0, 0, 0, 0.75)", // This will give the overlay a black color with 75% opacity
-            },
-            content: {
-              top: "50%",
-              left: "55%",
-              right: "auto",
-              bottom: "auto",
-              marginRight: "-50%",
-              transform: "translate(-50%, -50%)",
-              backgroundColor: "#fff", // This will give the modal a white background
-              border: "1px solid #ccc", // This will give the modal a border
-              borderRadius: "10px", // This will round the corners of the modal
-              padding: "20px", // This will add some padding inside the modal
-              width: "30%", // This will set the width of the modal to 30% of the viewport width
-              height: "30%", // This will set the height of the modal to 30% of the viewport height
-              display: "flex", // This will make the content a flex container
-              flexDirection: "column", // This will make the flex items stack vertically
-              justifyContent: "center", // This will center the flex items vertically
-              alignItems: "center", // This will center the flex items horizontally
-            },
-          }}
-        >
-          <h2 className="text-2xl font-Lato-black text-secondary-6 mb-2">
-            Are you sure you want to delete this devotion?
-          </h2>
-          <div>
-            <button
-              onClick={() => {
-                if (devotionToDelete) {
-                  handleDelete(devotionToDelete); // delete the devotion when "Yes" is clicked
-                }
-              }}
-              style={{
-                backgroundColor: "#ff0000", // red background
-                color: "#ffffff", // white text
-                border: "none", // remove border
-                borderRadius: "5px", // rounded corners
-                padding: "10px 20px", // vertical and horizontal padding
-                marginRight: "10px", // space between the buttons
-                cursor: "pointer", // change cursor on hover
-              }}
-            >
-              Yes
-            </button>
-            <button
-              onClick={() => setModalIsOpen(false)}
-              style={{
-                backgroundColor: "#1A2024", // green background
-                color: "#ffffff", // white text
-                border: "none", // remove border
-                borderRadius: "5px", // rounded corners
-                padding: "10px 20px", // vertical and horizontal padding
-                cursor: "pointer", // change cursor on hover
-              }}
-            >
-              No
-            </button>
-          </div>
-        </Modal>
-        {devotionToDisplay &&
-        (devotionToDisplay.month !== "" || devotionToDisplay.day !== "") ? (
-          <div className="rounded-xl w-[20%] h-full border-2 bg-[#fff] border-accent-5 mt-8 text-secondary-6">
-            <div className="w-[95%] h-[95%] mx-auto  flex flex-col justify-center items-center border-2 bg-secondary-6  rounded-xl my-1 leading-none  py-6">
-              <p className=" font-nokia-bold text-3xl text-[#fff]">
-                {devotionToDisplay.month}
-              </p>
-              <p className="text-5xl font-nokia-bold text-[#fff]">
-                {devotionToDisplay.day}
-              </p>
-            </div>
-          </div>
-        ) : (
-          <div className="hidden rounded-xl w-[20%] h-full border-2 bg-[#fff] border-accent-5 mt-8 text-secondary-6">
-            <div className="w-[90%] mx-auto h-[95%] flex flex-col justify-center items-center border-2 bg-secondary-6   rounded-xl my-1 leading-none py-6">
-              <p className="font-nokia-bold text-3xl text-[#fff]">
-                {devotionToDisplay && devotionToDisplay.month}
-              </p>
-              <p className="font-nokia-bold text-5xl text-[#fff]">
-                {devotionToDisplay && devotionToDisplay.day}
-              </p>
-            </div>
-          </div>
-        )}
-
-        <div className="font-nokia-bold flex flex-col w-[50%] space-y-2 mt-8">
-          <div className="flex width: 100% space-x-12">
-            <h1 className="text-4xl text-justify text-secondary-6">
-              {devotionToDisplay && devotionToDisplay.title}
-            </h1>
-            {role === "Admin" && showControls && (
-              <>
-                <FaTrash
-                  className="text-gray-700 text-xl cursor-pointer self-center"
-                  onClick={() => {
-                    setDevotionToDelete(devotionToDisplay?._id || ""); // set the id to delete
-                    setModalIsOpen(true); // open the modal
-                  }}
-                />
-                <FaEdit
-                  className="text-gray-700 text-xl cursor-pointer self-center "
-                  onClick={() => startEditing(devotionToDisplay)}
-                />
-              </>
-            )}
-          </div>
-          <h4 className="text-1xl text-secondary-6">
-            የዕለቱ የመጽሐፍ ቅዱስ ንባብ ክፍል-{" "}
-          </h4>
-          <h2 className=" text-lg text-accent-5">
-            {devotionToDisplay && devotionToDisplay.chapter}
-          </h2>
-          <p className=" text-1xl text-secondary-6">
-            {devotionToDisplay && devotionToDisplay.verse}
-          </p>
-
-          {devotionToDisplay && devotionToDisplay.chapter !== "" ? (
-            <hr className="border-accent-5" />
-          ) : (
-            <hr className="hidden border-secondary-6" />
-          )}
-
-          {devotionToDisplay &&
-            devotionToDisplay.body.map((paragraph, paragraphIndex) => (
-              <p
-                className=" font-nokia-bold text-sm text-justify text-secondary-6 space-y-3"
-                key={paragraphIndex}
+    <>
+      <div className="h-auto border-2 shadow-lg rounded-2xl p-6">
+        <div>
+          <h1 className="font-nokia-bold  text-3xl text-accent-5 ">
+            Daily Devotional
+          </h1>
+        </div>
+        <div className="flex space-x-12">
+          {/* Replace latestDevotion with devotionToDisplay */}
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={() => setModalIsOpen(false)}
+            contentLabel="Delete Confirmation"
+            style={{
+              overlay: {
+                backgroundColor: "rgba(0, 0, 0, 0.75)", // This will give the overlay a black color with 75% opacity
+              },
+              content: {
+                top: "50%",
+                left: "55%",
+                right: "auto",
+                bottom: "auto",
+                marginRight: "-50%",
+                transform: "translate(-50%, -50%)",
+                backgroundColor: "#fff", // This will give the modal a white background
+                border: "1px solid #ccc", // This will give the modal a border
+                borderRadius: "10px", // This will round the corners of the modal
+                padding: "20px", // This will add some padding inside the modal
+                width: "30%", // This will set the width of the modal to 30% of the viewport width
+                height: "30%", // This will set the height of the modal to 30% of the viewport height
+                display: "flex", // This will make the content a flex container
+                flexDirection: "column", // This will make the flex items stack vertically
+                justifyContent: "center", // This will center the flex items vertically
+                alignItems: "center", // This will center the flex items horizontally
+              },
+            }}
+          >
+            <h2 className="text-2xl font-Lato-black text-secondary-6 mb-2">
+              Are you sure you want to delete this devotion?
+            </h2>
+            <div>
+              <button
+                onClick={() => {
+                  if (devotionToDelete) {
+                    handleDelete(devotionToDelete); // delete the devotion when "Yes" is clicked
+                  }
+                }}
+                style={{
+                  backgroundColor: "#ff0000", // red background
+                  color: "#ffffff", // white text
+                  border: "none", // remove border
+                  borderRadius: "5px", // rounded corners
+                  padding: "10px 20px", // vertical and horizontal padding
+                  marginRight: "10px", // space between the buttons
+                  cursor: "pointer", // change cursor on hover
+                }}
               >
-                {paragraph}
-              </p>
-            ))}
-
-          {devotionToDisplay && devotionToDisplay.prayer !== "" ? (
-            <p className="font-nokia-bold text-1xl text-center border-2 border-accent-5 p-2 rounded text-accent-5">
-              {devotionToDisplay.prayer}
-            </p>
-          ) : (
-            <p className="hidden font-nokia-bold text-1xl text-center border-2 border-accent-5 p-2 rounded text-accent-5">
-              {devotionToDisplay && devotionToDisplay.prayer}
-            </p>
-          )}
-        </div>
-
-        <div className="w-[25%] mt-12 flex flex-col space-y-6">
-          <img
-            src={`https://ezra-seminary.mybese.tech/images/${
-              devotionToDisplay && devotionToDisplay.image
-            }`}
-            alt="Devotion Image"
-          />
-
+                Yes
+              </button>
+              <button
+                onClick={() => setModalIsOpen(false)}
+                style={{
+                  backgroundColor: "#1A2024", // green background
+                  color: "#ffffff", // white text
+                  border: "none", // remove border
+                  borderRadius: "5px", // rounded corners
+                  padding: "10px 20px", // vertical and horizontal padding
+                  cursor: "pointer", // change cursor on hover
+                }}
+              >
+                No
+              </button>
+            </div>
+          </Modal>
           {devotionToDisplay &&
-            typeof devotionToDisplay.previewUrl === "string" && (
-              <img src={devotionToDisplay.previewUrl} alt="Preview" />
+          (devotionToDisplay.month !== "" || devotionToDisplay.day !== "") ? (
+            <div className="rounded-xl w-[20%] h-full border-2 bg-[#fff] border-accent-5 mt-8 text-secondary-6">
+              <div className="w-[95%] h-[95%] mx-auto  flex flex-col justify-center items-center border-2 bg-secondary-6  rounded-xl my-1 leading-none  py-6">
+                <p className=" font-nokia-bold text-3xl text-[#fff]">
+                  {devotionToDisplay.month}
+                </p>
+                <p className="text-5xl font-nokia-bold text-[#fff]">
+                  {devotionToDisplay.day}
+                </p>
+              </div>
+            </div>
+          ) : (
+            <div className="hidden rounded-xl w-[20%] h-full border-2 bg-[#fff] border-accent-5 mt-8 text-secondary-6">
+              <div className="w-[90%] mx-auto h-[95%] flex flex-col justify-center items-center border-2 bg-secondary-6   rounded-xl my-1 leading-none py-6">
+                <p className="font-nokia-bold text-3xl text-[#fff]">
+                  {devotionToDisplay && devotionToDisplay.month}
+                </p>
+                <p className="font-nokia-bold text-5xl text-[#fff]">
+                  {devotionToDisplay && devotionToDisplay.day}
+                </p>
+              </div>
+            </div>
+          )}
+
+          <div className="font-nokia-bold flex flex-col w-[50%] space-y-2 mt-8">
+            <div className="flex width: 100% space-x-12">
+              <h1 className="text-4xl text-justify text-secondary-6">
+                {devotionToDisplay && devotionToDisplay.title}
+              </h1>
+              {role === "Admin" && showControls && (
+                <>
+                  <FaTrash
+                    className="text-gray-700 text-xl cursor-pointer self-center"
+                    onClick={() => {
+                      setDevotionToDelete(devotionToDisplay?._id || ""); // set the id to delete
+                      setModalIsOpen(true); // open the modal
+                    }}
+                  />
+                  <FaEdit
+                    className="text-gray-700 text-xl cursor-pointer self-center "
+                    onClick={() => startEditing(devotionToDisplay)}
+                  />
+                </>
+              )}
+            </div>
+            <h4 className="text-1xl text-secondary-6">
+              የዕለቱ የመጽሐፍ ቅዱስ ንባብ ክፍል-{" "}
+            </h4>
+            <h2 className=" text-lg text-accent-5">
+              {devotionToDisplay && devotionToDisplay.chapter}
+            </h2>
+            <p className=" text-1xl text-secondary-6">
+              {devotionToDisplay && devotionToDisplay.verse}
+            </p>
+
+            {devotionToDisplay && devotionToDisplay.chapter !== "" ? (
+              <hr className="border-accent-5" />
+            ) : (
+              <hr className="hidden border-secondary-6" />
             )}
 
-          {devotionToDisplay && devotionToDisplay.previewUrl ? (
-            <img src="../../assets/Advert-Image.svg" alt="" />
-          ) : (
+            {devotionToDisplay &&
+              devotionToDisplay.body.map((paragraph, paragraphIndex) => (
+                <p
+                  className=" font-nokia-bold text-sm text-justify text-secondary-6 space-y-3"
+                  key={paragraphIndex}
+                >
+                  {paragraph}
+                </p>
+              ))}
+
+            {devotionToDisplay && devotionToDisplay.prayer !== "" ? (
+              <p className="font-nokia-bold text-1xl text-center border-2 border-accent-5 p-2 rounded text-accent-5">
+                {devotionToDisplay.prayer}
+              </p>
+            ) : (
+              <p className="hidden font-nokia-bold text-1xl text-center border-2 border-accent-5 p-2 rounded text-accent-5">
+                {devotionToDisplay && devotionToDisplay.prayer}
+              </p>
+            )}
+          </div>
+
+          <div className="w-[25%] mt-12 flex flex-col space-y-6">
             <img
-              src="../../assets/Advert-Image.svg"
-              alt=""
-              className="hidden"
+              src={`https://ezra-seminary.mybese.tech/images/${
+                devotionToDisplay && devotionToDisplay.image
+              }`}
+              alt="Devotion Image"
             />
-          )}
+
+            {devotionToDisplay &&
+              typeof devotionToDisplay.previewUrl === "string" && (
+                <img src={devotionToDisplay.previewUrl} alt="Preview" />
+              )}
+
+            {devotionToDisplay && devotionToDisplay.previewUrl ? (
+              <img src="../../assets/Advert-Image.svg" alt="" />
+            ) : (
+              <img
+                src="../../assets/Advert-Image.svg"
+                alt=""
+                className="hidden"
+              />
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
