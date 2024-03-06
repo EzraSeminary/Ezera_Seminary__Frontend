2aa9390b00c9bb10f9c0dea7bef447f55b56dd93
Besufekad-HAZ <besurye@gmail.com>
2024-03-06 17:32:51 +0300

    Add login animation

---
+++ src/pages/user/LoadingAnimtaion.tsx
@@ -0,0 +1,17 @@
+import "./LoadingAnimation.css"; // Make sure to create this CSS file
+
+const LoadingAnimation = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading-animation">
+        {/* You can add your custom animated elements here */}
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+      </div>
+      <p className="loading-text">Logging you in...</p>
+    </div>
+  );
+};
+
+export default LoadingAnimation;
