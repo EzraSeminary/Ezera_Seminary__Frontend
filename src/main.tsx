042b97a2366ffe5b53fc49bd3eda2cf8bae63aec
yeabtsega45 <yabtsega2022@gmail.com>
2024-03-12 17:13:24 +0300

    adding react-error-boundary

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 import axios from "axios";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { ErrorBoundary } from "react-error-boundary";
 
 // axios.defaults.baseURL = "http://localhost:5100";
 axios.defaults.baseURL = "https://ezra-seminary.mybese.tech";
@@ -13,8 +14,10 @@ axios.defaults.headers.common["Content-Type"] = "multipart/form-data";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
