f665c4ea01455b664668cfedf32e951c5f9ab654
Besufekad-HAZ <besurye@gmail.com>
2024-03-13 12:23:14 +0300

    Add Avatars and new layout in the profile settings page

--- src/pages/user/ProfileSettings.tsx
+++ src/pages/user/ProfileSettings.tsx
@@ -34,7 +34,7 @@ const ProfileSettings = () => {
     setShowPassword(!showPassword);
   };
 
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
+  const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
 
     // Add a type guard to ensure currentUser is not undefined
@@ -57,74 +57,98 @@ const ProfileSettings = () => {
   };
 
   return (
-    <div className=" my-10 max-w-lg mx-auto p-6 bg-accent-3 rounded-lg shadow-md">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-primary-1 p-6 rounded-lg shadow-md"
-      >
-        <div className="mb-4">
-          <label className="block text-secondary-10 text-sm font-bold mb-2">
-            First Name:
-          </label>
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline"
+    <>
+      <div className="container mx-auto my-10 p-6 bg-white rounded-lg shadow-md"></div>
+      <div className="md:flex md:items-start md:space-x-6">
+        <div className="md:w-1/3 text-center mb-6 md:mb-0">
+          <img
+            // src={currentUser.avatar || "default-avatar.png"} // Replace with your default avatar image path
+            alt="User Avatar"
+            className="w-24 h-24 rounded-full mx-auto"
           />
-        </div>
-        <div className="mb-4">
-          <label className="block text-secondary-10 text-sm font-bold mb-2">
-            Last Name:
-          </label>
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-secondary-10 text-sm font-bold mb-2">
-            Email:
-          </label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-6 relative">
-          <label className="block text-secondary-10 text-sm font-bold mb-2">
-            Password:
-          </label>
-          <input
-            type={showPassword ? "text" : "password"}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline pr-10"
-          />
-          <div
-            className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
-            onClick={toggleShowPassword}
-          >
-            <FontAwesomeIcon
-              icon={showPassword ? faEyeSlash : faEye}
-              className="text-secondary-10 mt-8"
-            />
+          <div className="mt-4">
+            <h3 className="text-lg font-semibold">{`${firstName} ${lastName}`}</h3>
+            <label className="mt-2 inline-block bg-accent-6 text-white py-1 px-4 rounded cursor-pointer hover:bg-accent-7">
+              <span>Upload New Photo</span>
+              <input
+                type="file"
+                className="hidden"
+                // onChange={handleAvatarUpload}
+              />{" "}
+              {/* Implement handleAvatarUpload */}
+            </label>
           </div>
         </div>
-        <div className="flex items-center justify-between">
-          <button
-            type="submit"
-            className="bg-accent-6 hover:bg-accent-7 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            Update
-          </button>
+        <div className="md:w-2/3">
+          <h2 className="text-2xl font-bold text-secondary-10 mb-6">
+            Edit Profile
+          </h2>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="mb-4">
+              <label className="block text-secondary-10 text-sm font-bold mb-2">
+                First Name:
+              </label>
+              <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-secondary-10 text-sm font-bold mb-2">
+                Last Name:
+              </label>
+              <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-secondary-10 text-sm font-bold mb-2">
+                Email:
+              </label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            <div className="mb-6 relative">
+              <label className="block text-secondary-10 text-sm font-bold mb-2">
+                Password:
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline pr-10"
+              />
+              <div
+                className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+                onClick={toggleShowPassword}
+              >
+                <FontAwesomeIcon
+                  icon={showPassword ? faEyeSlash : faEye}
+                  className="text-secondary-10 mt-8"
+                />
+              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                type="submit"
+                className="bg-accent-6 hover:bg-accent-7 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Update Info
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
+      </div>
+    </>
   );
 };
 
