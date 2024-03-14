fe03be35bbc75ed01b42f3e359462f802ff7eea6
Yidne <Yamiyekn@gmail.com>
2024-03-14 13:22:04 +0300

    Update Footer component styling and content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,13 @@ const Footer = () => {
         <div className="flex flex-col bg-secondary-6 justify-center items-center py-8 space-y-4">
           <div className="flex flex-row justify-center items-center space-x-3">
             <div>
-              <img src="../assets/ezra-logo.svg" alt="" />
+              <img src="src/assets/Logo Ion.png" alt="" className="w-7 lg:w-9 xl:w-11" />
             </div>
             <div className="flex flex-col">
-              <h1 className="text-[#fff] ">
-                <strong className="text-2xl">Ezra</strong> Seminary
+              <h1 className="text-[#fff] xl:text-xl">
+                <strong className="text-lg lg:text-2xl xl:text-3xl">Ezra</strong> Seminary
               </h1>
-              <p className="text-accent-5 text-base font-nokia-light">
+              <p className="text-accent-5 text-sm xl:text-xl -mt-1 lg:text-base font-nokia-light">
                 Spreading the Gospel
               </p>
             </div>
@@ -21,19 +21,19 @@ const Footer = () => {
               <div>
                 <img src="../assets/message-icon.svg" alt="" />
               </div>
-              <p className="font-nokia-light">ezraseminary@gmail.com</p>
+              <p className="font-nokia-light text-sm xl:text-xl">ezraseminary@gmail.com</p>
             </div>
             <div className="flex flex-row space-x-2 justify-center items-center">
               <div>
                 <img src="../assets/phone-icon.svg" alt="" />
               </div>
-              <p className="font-nokia-light">+251 911 12 13 14</p>
+              <p className="font-nokia-light text-sm xl:text-xl ">+251 911 12 13 14</p>
             </div>
           </div>
         </div>
         <div className=" bg-[#293239] w-[100%]">
           <div className="flex flex-col justify-center items-center py-6 space-y-6 md:flex-row md:justify-between md:w-[80%] mx-auto">
-            <p className="font-nokia-light text-[#fff] w-[40%] text-center md:w-[25%] md:text-left">
+            <p className="font-nokia-light text-sm lg:text-md xl:text-xl text-[#fff] w-[70%] text-center md:w-[45%] md:text-left">
               Copyright 2024 @ YetinbitKal Ministry Website by{" "}
               <span className="text-accent-5  font-nokia-bold"> AmenDevs </span>
             </p>
