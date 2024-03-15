ad992fa520a250a482bf206a84342c2f6dd59846
Besufekad-HAZ <besurye@gmail.com>
2024-03-15 15:52:57 +0300

    Remove uneccessary console logs for deployement

--- src/pages/user/ProfileSettings.tsx
+++ src/pages/user/ProfileSettings.tsx
@@ -73,6 +73,8 @@ const ProfileSettings = () => {
           }
 
           const updatedUser = await updateUserMutation(formData).unwrap();
+          toast.success("Profile updated successfully!");
+          // console.log("Mutation successful, updatedUser:", updatedUser);
           dispatch(updateUser(updatedUser));
           setFirstName(updatedUser.firstName);
           setLastName(updatedUser.lastName);
@@ -87,7 +89,6 @@ const ProfileSettings = () => {
 
           // Save the updated user information to local storage
           localStorage.setItem("user", JSON.stringify(updatedUser));
-          toast.success("Profile updated successfully!");
         } catch (error: unknown) {
           if (
             typeof error === "object" &&
@@ -105,11 +106,14 @@ const ProfileSettings = () => {
               apiError.status === 400 &&
               apiError.data.message === "Error uploading avatar"
             ) {
+              console.error("Mutation failed:", error);
               toast.error("Failed to upload avatar. Please try again.");
             } else {
+              console.error("Mutation failed:", error);
               toast.error("Failed to update profile. Please try again.");
             }
           } else {
+            console.error("Mutation failed:", error);
             toast.error("An unknown error occurred. Please try again.");
           }
         }
@@ -122,112 +126,114 @@ const ProfileSettings = () => {
   };
 
   return (
-    <div className="container mx-8 my-20 p-10 bg-accent-1 rounded-lg shadow-lg">
+    <>
       <ToastContainer />
-      <button
-        onClick={goBack}
-        className="mb-4 bg-accent-6 hover:bg-accent-7 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
-      >
-        <ArrowLeft size={25} />
-        Back
-      </button>
-      <div className="md:flex md:items-start md:space-x-10">
-        <div className="md:w-1/3 text-center mb-6 md:mb-0">
-          <img
-            src={
-              avatarPreview
-                ? avatarPreview
-                : currentUser?.avatar
-                ? `https://ezra-seminary.mybese.tech/images/${currentUser.avatar}`
-                : mehari
-            }
-            alt="User Avatar"
-            className="w-[25vmin] rounded-full mx-auto"
-          />
-          <div className="mt-4">
-            <h3 className="text-lg font-semibold">{`${firstName} ${lastName}`}</h3>
-            <label className="mt-2 inline-block bg-accent-6 text-white py-1 px-4 rounded cursor-pointer hover:bg-accent-7">
-              <span>Upload New Photo</span>
-              <input
-                type="file"
-                className="hidden"
-                onChange={handleAvatarUpload}
-              />
-            </label>
+      <div className="container mx-8 my-20 p-10 bg-accent-1 rounded-lg shadow-lg">
+        <button
+          onClick={goBack}
+          className="mb-4 bg-accent-6 hover:bg-accent-7 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+        >
+          <ArrowLeft size={25} />
+          Back
+        </button>
+        <div className="md:flex md:items-start md:space-x-10">
+          <div className="md:w-1/3 text-center mb-6 md:mb-0">
+            <img
+              src={
+                avatarPreview
+                  ? avatarPreview
+                  : currentUser?.avatar
+                  ? `https://ezra-seminary.mybese.tech/images/${currentUser.avatar}`
+                  : mehari
+              }
+              alt="User Avatar"
+              className="w-[25vmin] rounded-full mx-auto"
+            />
+            <div className="mt-4">
+              <h3 className="text-lg font-semibold">{`${firstName} ${lastName}`}</h3>
+              <label className="mt-2 inline-block bg-accent-6 text-white py-1 px-4 rounded cursor-pointer hover:bg-accent-7">
+                <span>Upload New Photo</span>
+                <input
+                  type="file"
+                  className="hidden"
+                  onChange={handleAvatarUpload}
+                />
+              </label>
+            </div>
+          </div>
+          {/* Second Section - Edit Profile Form */}
+          <div className="md:w-2/3">
+            <h2 className="text-2xl font-bold text-secondary-10 mb-6">
+              Edit Profile
+            </h2>
+            <form onSubmit={handleSubmit} className="space-y-6">
+              <div className="mb-4">
+                <label className="block text-secondary-10 text-sm font-bold mb-2">
+                  First Name:
+                </label>
+                <input
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-secondary-10 text-sm font-bold mb-2">
+                  Last Name:
+                </label>
+                <input
+                  type="text"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-secondary-10 text-sm font-bold mb-2">
+                  Email:
+                </label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
+              <div className="mb-6 relative">
+                <label className="block text-secondary-10 text-sm font-bold mb-2">
+                  Password:
+                </label>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline pr-10"
+                />
+                <div
+                  className="absolute inset-y-0 right-0 pt-8 pr-3 flex items-center cursor-pointer text-accent-8"
+                  onClick={toggleShowPassword}
+                >
+                  {showPassword ? (
+                    <EyeSlash size={18} weight="fill" />
+                  ) : (
+                    <Eye size={18} weight="fill" />
+                  )}
+                </div>
+              </div>
+              <div className="flex items-center justify-between">
+                <button
+                  type="submit"
+                  className="bg-accent-6 hover:bg-accent-7 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Update Info
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-        {/* Second Section - Edit Profile Form */}
-        <div className="md:w-2/3">
-          <h2 className="text-2xl font-bold text-secondary-10 mb-6">
-            Edit Profile
-          </h2>
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <div className="mb-4">
-              <label className="block text-secondary-10 text-sm font-bold mb-2">
-                First Name:
-              </label>
-              <input
-                type="text"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-secondary-10 text-sm font-bold mb-2">
-                Last Name:
-              </label>
-              <input
-                type="text"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-secondary-10 text-sm font-bold mb-2">
-                Email:
-              </label>
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline"
-              />
-            </div>
-            <div className="mb-6 relative">
-              <label className="block text-secondary-10 text-sm font-bold mb-2">
-                Password:
-              </label>
-              <input
-                type={showPassword ? "text" : "password"}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-secondary-10 leading-tight focus:outline-none focus:shadow-outline pr-10"
-              />
-              <div
-                className="absolute inset-y-0 right-0 pt-8 pr-3 flex items-center cursor-pointer text-accent-8"
-                onClick={toggleShowPassword}
-              >
-                {showPassword ? (
-                  <EyeSlash size={18} weight="fill" />
-                ) : (
-                  <Eye size={18} weight="fill" />
-                )}
-              </div>
-            </div>
-            <div className="flex items-center justify-between">
-              <button
-                type="submit"
-                className="bg-accent-6 hover:bg-accent-7 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              >
-                Update Info
-              </button>
-            </div>
-          </form>
-        </div>
       </div>
-    </div>
+    </>
   );
 };
 
