4ebe5e509b33eba5051dc85a7c41287dd0c6157d
AmanWorku <amexworku2014@gmail.com>
2024-03-18 16:47:50 +0300

    Add new features and update UI in LoggedInHome component

--- src/features/home/LoggedInHome.tsx
+++ src/features/home/LoggedInHome.tsx
@@ -1,5 +1,37 @@
-import { BookOpenText } from "@phosphor-icons/react";
+import { BookOpenText, ArrowSquareUpRight } from "@phosphor-icons/react";
 import bible from "../../assets/bible.png";
+// import DateConverter from "../sabbathSchool/DateConverter";
+
+const sampleData = [
+    {
+      _id: '1',
+      title: 'Devotion 1',
+      month: 'January',
+      day: '1',
+      image: 'image1.jpg'
+    },
+    {
+      _id: '2',
+      title: 'Devotion 2',
+      month: 'February',
+      day: '15',
+      image: 'image2.jpg'
+    },
+    {
+      _id: '3',
+      title: 'Devotion 3',
+      month: 'March',
+      day: '22',
+      image: 'image3.jpg'
+    },
+    {
+      _id: '4',
+      title: 'Devotion 4',
+      month: 'April',
+      day: '10',
+      image: 'image4.jpg'
+    }
+  ];
 
 const LoggedInHome = () => {
     return (
@@ -55,7 +87,7 @@ const LoggedInHome = () => {
               የአጠናን ዘዴዎች
             </p>
             <p
-              className=" text-2xl mt-2 leading-tight">
+              className=" text-2xl leading-tight">
               ፍሬያማ የመጽሃፍ ቅዱስ አጠናን ዘዴዎች
             </p>
             <button
@@ -66,6 +98,85 @@ const LoggedInHome = () => {
               </p>
             </button>
           </div>
+          <div className="flex flex-row justify-between items-center mt-4">
+            <p
+              className="font-nokia-bold text-secondary-4 text-lg">
+              Study this week's SSL
+            </p>
+            <button
+              className="border border-accent-6 px-4 py-1 rounded-full">
+              <p className="font-nokia-bold text-accent-6 text-sm">
+                All SSLs
+              </p>
+            </button>
+          </div>
+          <div className="rounded-2 overflow-hidden">
+  <div className="flex flex-row border border-accent-6 mt-4 rounded-lg p-2 gap-2">
+    <div className="h-auto w-32">
+      <img
+        src={bible}
+        alt="Background"
+        className="w-full h-full rounded-lg"
+      />
+    </div>
+    <div className="w-65%">
+      <h3 className="font-nokia-bold text-accent-6 text-sm leading-tight">
+      መጽሐፈ መዝሙረ ዳዊት
+      </h3>
+      <h2 className="font-nokia-bold text-lg leading-tight text-secondary-6">
+      መጨረሻ የለሌው አምልኮ
+      </h2>
+      <div className="border-b border-accent-6 mt-1" />
+      <p className="font-nokia-bold text-xs mt-2 text-secondary-5">
+        <div className="flex flex-row items-center">
+          {/* <DateConverter
+            gregorianDate="2023-05-01"
+          />
+          <span className="font-nokia-bold text-secondary-5"> - </span>
+          <DateConverter
+            gregorianDate="2023-05-07"
+          /> */}
+          <p>መጋቢት 6 - መጋቢት 12</p>
+        </div>
+      </p>
+      <button className="bg-accent-6 px-4 py-1 rounded-full w-36 mt-1">
+        <span className="text-primary-1 font-nokia-bold text-sm text-center">
+          ትምህርቱን ክፈት
+        </span>
+      </button>
+    </div>
+  </div>
+</div>
+<div className="flex flex-row flex-wrap justify-between mt-4">
+  {sampleData.slice(0, 4).map((item, index) => (
+    <div
+      key={index}
+      className="w-[47.5%] h-35 mb-4 rounded-2 overflow-hidden relative"
+    >
+      <img
+        src={item.image}
+        alt={item.title}
+        className="w-full h-full object-cover rounded-lg"
+      />
+      <div className="absolute inset-0 bg-accent-10 bg-opacity-60 rounded-lg">
+        <ArrowSquareUpRight
+          size={32}
+          weight="fill"
+          style={{ color: '#F8F8F8' }}
+          className="text-white self-end m-2"
+        />
+        <div className="absolute bottom-0 left-0 my-2">
+          <h3 className="font-nokia-bold text-white text-lg mx-2">
+            {item.title}
+          </h3>
+          <p className="font-nokia-bold text-sm mx-2 text-accent-2">
+            {item.month} {item.day}
+          </p>
+        </div>
+      </div>
+    </div>
+  ))}
+</div>
         </div>
     )
 }
