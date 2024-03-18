43e5e9ebdecb3aff867b1c00ec1c752ae67fa7ee
AmanWorku <amexworku2014@gmail.com>
2024-03-18 13:50:03 +0300

    Update styling in LoggedInHome component

--- src/features/home/LoggedInHome.tsx
+++ src/features/home/LoggedInHome.tsx
@@ -2,8 +2,8 @@ import { BookOpenText } from "@phosphor-icons/react";
 
 const LoggedInHome = () => {
     return (
-        <div className="w-90% p-4">
-            <p className="text-2xl font-nokia-bold text-accent-6 border-b border-accent-6 pb-2">Daily Devotionals</p>
+        <div className="w-90% p-4 font-nokia-bold text-secondary-6">
+            <p className="text-2xl  text-accent-6 border-b border-accent-6 pb-2">Daily Devotionals</p>
         <div
             className="border-2 border-accent-6 mt-6 rounded-lg bg-primary-2 shadow-lg px-4 py-4">
             <div
@@ -15,13 +15,13 @@ const LoggedInHome = () => {
                   className="text-accent-6"
                   />
                 <p
-                  className="text-secondary-6 font-nokia-bold text-lg">
+                  className=" font-nokia-bold text-lg">
                   የዕለቱ ጥቅስ
                 </p>
               </div>
               <button
                 className="bg-accent-6 px-4 py-1 rounded-full">
-                <p className="text-primary-1 font-nokia-bold text-sm">
+                <p className="text-primary-1 text-sm">
                   Open
                 </p>
               </button>
@@ -29,10 +29,19 @@ const LoggedInHome = () => {
             <div className="border-b border-accent-6 mt-2 mb-1" />
             <div>
               <p
-                className="font-nokia-bold text-lg text-secondary-6">
+                className="text-lg">
                 ”ስምህ እግዚአብሔር የሆነው አንተ ብቻ፣ በምድር ሁሉ ላይ ልዑል እንደ ሆንህ ይወቁ።“ መዝሙር 83:18
               </p>
             </div>
+          </div>
+          <div className="flex flex-row justify-between items-center mt-4 border-t border-secondary-3 pt-4">
+            <p>Continue Studying</p>
+            <button
+                className="border border-accent-6 px-4 py-1 rounded-full">
+                <p className="text-accent-6 text-sm">
+                  All Courses
+                </p>
+              </button>
           </div>
         </div>
     )
