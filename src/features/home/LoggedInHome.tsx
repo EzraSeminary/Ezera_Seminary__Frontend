616c307c9792815ead0db40da144413967160f57
Yidne <Yamiyekn@gmail.com>
2024-03-27 10:42:16 +0300

    Update loggedInHome component styling.

--- src/features/home/LoggedInHome.tsx
+++ src/features/home/LoggedInHome.tsx
@@ -75,124 +75,15 @@ const LoggedInHome = () => {
   };
 
   return (
-    <div className="w-[90%] py-12 space-y-6 font-nokia-bold text-secondary-6 mx-auto lg:w-[90%]">
-      <p className="text-xl lg:text-3xl  text-accent-6 border-b border-accent-6 pb-2">
-        Daily Devotionals
-      </p>
-      {/* Today's verse */}
-      <div className=" w-full mx-auto flex flex-col border-2 border-accent-6 mt-6 rounded-lg bg-primary-2 shadow-2xl p-4  space-y-6 lg:flex-row lg:items-start lg:gap-6 lg:justify-between lg:w-[90%]">
-        <div className="h-48 md:w-[35%] lg:h-52  xl:h-64">
-          <img
-            src={
-              `https://ezra-seminary.mybese.tech/images/${latestDevotion.image}` ||
-              `${bible}`
-            }
-            className="w-full  h-full object-cover rounded-lg"
-          />
-        </div>
-        <div className="lg:w-[65%] lg:space-y-3">
-          <div className="flex flex-row w-[100%] justify-between items-center">
-            <div className="flex flex-row items-center gap-2">
-              <BookOpenText size={32} weight="bold" className="text-accent-6" />
-              <p className=" font-nokia-bold text-lg xl:text-xl">የዕለቱ ጥቅስ</p>
-            </div>
-            <button
-              className="bg-accent-6 px-4 py-1 rounded-full"
-              onClick={handleOpenDevotion}
-            >
-              <p className="text-primary-1 text-sm xl:text-lg">Open</p>
-            </button>
-          </div>
-          <div className="border-b border-accent-6 mt-2 mb-1" />
-          <div>
-            <p className="text-sm xl:text-lg">{latestDevotion.verse}</p>
-          </div>
-        </div>
-      </div>
-
-      {/* Continue studying */}
-      <div className="flex flex-row justify-between items-center mt-4 border-t border-secondary-3 pt-4">
-        <p className="text-lg lg:text-xl xl:text-2xl">Continue Studying</p>
-        <button className="border border-accent-6 px-4 py-1 rounded-full">
-          <p className="text-accent-6 text-sm">All Courses</p>
-        </button>
-      </div>
-      {/* Continue studying contents */}
-      <div className="w-full mx-auto  flex flex-col shadow-2xl   border border-accent-6 mt-4 rounded-lg p-4 lg:p-0 space-y-6 lg:space-y-0 lg:flex-row lg:items-start lg:justify-between   lg:w-[90%]">
-        <div className="h-48  lg:h-52  xl:h-64 lg:w-[65%]">
-          <img
-            src={bibleNew}
-            className="w-full h-full object-cover rounded-lg"
-          />
-        </div>
-        <div className="space-y-2 md:space-y-3 lg:w-[35%]   lg:flex lg:flex-col lg:justify-center lg:items-center lg:h-52  xl:h-64">
-          <p className=" text-accent-6 text-lg xl:text-2xl  leading-tight">
-            የአጠናን ዘዴዎች
+    <div className="w-[90%]  space-y-12 py-12 font-nokia-bold text-secondary-6 mx-auto lg:w-[90%] lg:space-y-20 lg:py-16 xl:py-24">
+      {/*daily devotions and course */}
+      <div className="grid grid-cols-1 space-y-6  items-start justify-start w-full gap-2 md:grid-cols-2 md:space-y-0  lg:w-[85%] xl:w-[90%] mx-auto">
+        {/* daily devotionals conatiner*/}
+        <div className="w-[90%] mx-auto ">
+          <p className="text-lg lg:text-xl xl:text-3xl  text-accent-6 border-b border-accent-6 pb-2 lg:w-[50%]">
+            Daily<span className="text-secondary-6"> Devotionals</span>
           </p>
-          <p className=" text-lg xl:text-xl leading-tight">
-            ፍሬያማ የመጽሃፍ ቅዱስ አጠናን ዘዴዎች
-          </p>
-          <button className="bg-accent-6 px-4 py-2 rounded-full w-36 ">
-            <p className="text-primary-1 font-nokia-bold text-sm text-center">
-              ኮርሱን ክፈት
-            </p>
-          </button>
-        </div>
-      </div>
-
-      {/* Study this week's SSL */}
-      <div className="flex flex-row justify-between items-center mt-4">
-        <p className=" text-secondary-6 text-lg lg:text-xl xl:text-2xl">
-          Study this week's SSL
-        </p>
-        <button className="border border-accent-6 px-4 py-1 rounded-full">
-          <p className=" text-accent-6 text-sm">All SSLs</p>
-        </button>
-      </div>
-      <div className="flex flex-row shadow-2xl border border-accent-6 mt-4 rounded-lg p-4 gap-4 lg:gap-6  lg:w-[90%] mx-auto">
-        <div className="h-auto w-32 md:w-[35%] lg:h-52  xl:h-64 ">
-          <img
-            src={bible}
-            alt="Background"
-            className="w-full h-full object-cover rounded-lg"
-          />
-        </div>
-        <div className="w-[65%] space-y-1 md:space-y-2 lg:pt-6">
-          <h3 className=" text-accent-6 text-sm xl:text-lg leading-tight">
-            መጽሐፈ መዝሙረ ዳዊት
-          </h3>
-          <h2 className=" text-lg xl:text-xl leading-tight">መጨረሻ የለሌው አምልኮ</h2>
-          <div className="border-b border-accent-6" />
-          <p className="text-xs xl:text-sm text-secondary-5">
-            መጋቢት 6 - መጋቢት 12
-          </p>
-          <button className="bg-accent-6 px-4 py-1 xl:py-2 rounded-full w-36">
-            <span className="text-primary-1 text-sm  text-center">
-              ትምህርቱን ክፈት
-            </span>
-          </button>
-        </div>
-      </div>
-
-      {/* Discover Devotionals */}
-      <div className="flex flex-row justify-between items-center mt-4">
-        <h2 className=" text-secondary-6 text-lg lg:text-xl xl:text-2xl">
-          Discover Devotionals
-        </h2>
-        <button
-          className="border border-accent-6 px-4 py-1 rounded-full"
-          onClick={() => handleViewDevotion(devotions[0])}
-        >
-          <span className=" text-accent-6 text-sm">All Devotionals</span>
-        </button>
-      </div>
-      <motion.div
-        variants={gridContainerVariants}
-        initial="hidden"
-        animate="show"
-        className="flex flex-row flex-wrap justify-between mt-4 lg:w-[90%] mx-auto"
-      >
-        {devotions.slice(0, 4).map((devotion, index) => (
+          {/* Today's verse */}
           <motion.div
             variants={gridSquareVariants}
             whileHover={{
@@ -203,35 +94,211 @@ const LoggedInHome = () => {
               bounceDamping: 10,
               bounceStiffness: 600,
             }}
-            key={index}
-            className=" shadow-2xl w-[48%] gap-2 md:w-[49%] md:gap-3 lg:w-[32.5%] lg:gap-2 xl:gap-3 xl:w-[32.5%] mb-4 rounded-2 overflow-hidden relative cursor-pointer"
-            onClick={() => handleViewDevotion(devotion)}
+            className=" w-[90%] mx-auto flex flex-col h-full border border-accent-6 mt-6 rounded-lg bg-primary-2 shadow-2xl p-4  space-y-4 cursor-pointer"
           >
-            <img
-              src={
-                `https://ezra-seminary.mybese.tech/images/${devotion.image}` ||
-                `${bible}`
-              }
-              alt={devotion.title}
-              className="w-full h-full md:h-auto lg:h-48 xl:h-64 object-cover rounded-lg"
-            />
-            <div className="absolute inset-0 h-full bg-accent-10 bg-opacity-60 rounded-lg">
-              <ArrowSquareUpRight
-                weight="fill"
-                className="text-primary-1 text-lg md:text-xl lg:text-2xl xl:text-3xl absolute top-0 right-0 m-2"
+            <div className="h-48  lg:h-52  xl:h-64">
+              <img
+                src={
+                  `https://ezra-seminary.mybese.tech/images/${latestDevotion.image}` ||
+                  `${bible}`
+                }
+                className="w-full  h-full object-cover rounded-lg"
               />
-              <div className="absolute bottom-0 left-0 my-2 md:top-[65%] lg:top-[70%] xl:top-[73%]">
-                <h3 className=" text-primary-1 text-sm xl:text-lg mx-2">
-                  {devotion.title}
-                </h3>
-                <p className="text-xs xl:text-sm mx-2 text-accent-2">
-                  {devotion.month} {devotion.day}
+            </div>
+            <div className="space-y-2 lg:space-y-3 h-full">
+              <div className="flex flex-row w-[100%] justify-between items-center">
+                <div className="flex flex-row items-center gap-2">
+                  <BookOpenText
+                    // size={32}
+                    weight="bold"
+                    className="text-accent-6 text-2xl lg:text-3xl xl:text-4xl"
+                  />
+                  <p className=" font-nokia-bold text-sm lg:text-lg xl:text-2xl">
+                    የዕለቱ ጥቅስ
+                  </p>
+                </div>
+                <button
+                  className="bg-accent-6 px-2 lg:px-4 lg:py-1 rounded-full"
+                  onClick={handleOpenDevotion}
+                >
+                  <p className="text-primary-1 text-xs lg:text-sm xl:text-xl">
+                    Open
+                  </p>
+                </button>
+              </div>
+              <div className="border-b border-accent-6 mt-2 mb-1" />
+              <div>
+                <p className="text-xs lg:text-sm xl:text-lg">
+                  {latestDevotion.verse}
                 </p>
               </div>
             </div>
           </motion.div>
-        ))}
-      </motion.div>
+        </div>
+
+        {/* Discover Courses Container*/}
+        <div className="w-[90%] mx-auto ">
+          {/* Continue studying */}
+          <div className="flex flex-row justify-between items-center border-b border-accent-6 pb-1">
+            <p className="text-lg lg:text-xl xl:text-3xl">
+              <span className="text-accent-6">Continue</span> Studying
+            </p>
+            <button className="border border-accent-6 text-accent-6 text-xs xl:text-sm  hover:text-primary-1 hover:bg-accent-6 px-2 xl:px-3 xl:py-1 xl:mb-1 mb-2 rounded-full">
+              All Courses
+            </button>
+          </div>
+          {/* Continue studying contents */}
+          <motion.div
+            variants={gridSquareVariants}
+            whileHover={{
+              scale: 1.06,
+            }}
+            whileTap={{ scale: 0.9 }}
+            transition={{
+              bounceDamping: 10,
+              bounceStiffness: 600,
+            }}
+            className="w-[90%] mx-auto h-full  flex flex-col shadow-2xl   border border-accent-6 mt-6 rounded-lg p-4  space-y-4 cursor-pointer"
+          >
+            <div className="h-48  lg:h-52  xl:h-64 ">
+              <img
+                src={bibleNew}
+                className="w-full h-full object-cover rounded-lg"
+              />
+            </div>
+            <div className="space-y-2 lg:space-y-3 xl:space-y-2 h-full ">
+              <p className=" text-accent-6 text-sm lg:text-lg xl:text-2xl leading-tight md:leading-none md:mt-2 lg:mt-0 xl:mt-1">
+                የአጠናን ዘዴዎች
+              </p>
+              <p className="text-xs lg:text-sm xl:text-xl  leading-tight  lg:leading-none md:pb-2">
+                ፍሬያማ የመጽሃፍ ቅዱስ አጠናን ዘዴዎች
+              </p>
+              <button className="bg-accent-6 px-4 lg:py-1  rounded-full w-max ">
+                <p className="text-primary-1 font-nokia-bold text-xs lg:text-sm text-center">
+                  ኮርሱን ክፈት
+                </p>
+              </button>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+
+      {/* sabbath school and devotionals */}
+      <div className="grid grid-cols-1 space-y-6  items-start justify-start w-full gap-2 lg:space-y-12  lg:w-[85%] xl:w-[90%] mx-auto ">
+        {/* sabbath school container */}
+        <div className="w-[90%] mx-auto ">
+          {/* Study this week's SSL */}
+          <div className="flex flex-row justify-between items-center border-b border-accent-6 pb-1">
+            <p className=" text-secondary-6 text-lg lg:text-xl xl:text-3xl">
+              <span className="text-accent-6">Study</span> this week's SSL
+            </p>
+            <button className="mb-1 border border-accent-6 hover:bg-accent-6 hover:text-primary-1  px-2 rounded-full text-accent-6 text-xs xl:px-3 xl:py-1 xl:mb-1 xl:text-sm">
+              All SSLs
+            </button>
+          </div>
+          <motion.div
+            variants={gridSquareVariants}
+            whileHover={{
+              scale: 1.06,
+            }}
+            whileTap={{ scale: 0.9 }}
+            transition={{
+              bounceDamping: 10,
+              bounceStiffness: 600,
+            }}
+            className="flex flex-row shadow-2xl border border-accent-6 mt-6 rounded-lg p-4 gap-4 lg:gap-6   w-[90%] mx-auto cursor-pointer"
+          >
+            <div className="h-auto w-32 md:w-[40%] lg:w-[80%] lg:h-52  xl:h-64 ">
+              <img
+                src={bible}
+                alt="Background"
+                className="w-full h-full object-cover rounded-lg"
+              />
+            </div>
+            <div className="w-[65%] space-y-1 md:space-y-2 lg:pt-12 lg:text-center ">
+              <h3 className=" text-accent-6 text-xs xl:text-xl leading-tight">
+                መጽሐፈ መዝሙረ ዳዊት
+              </h3>
+              <h2 className=" text-sm xl:text-2xl leading-tight">
+                መጨረሻ የለሌው አምልኮ
+              </h2>
+              <div className="border-b border-accent-6 lg:w-[70%] lg:mx-auto" />
+              <p className="text-xs xl:text-lg text-secondary-5">
+                መጋቢት 6 - መጋቢት 12
+              </p>
+              <button className="bg-accent-6 px-3  lg:py-1 rounded-full w-max">
+                <span className="text-primary-1 text-xs lg:text-sm  text-center">
+                  ትምህርቱን ክፈት
+                </span>
+              </button>
+            </div>
+          </motion.div>
+        </div>
+
+        {/* Discover Devotionals Container */}
+        <div className="w-[90%] mx-auto ">
+          {/* Discover Devotionals */}
+          <div className="flex flex-row justify-between items-start border-b border-accent-6 xl:items-center xl:mt-1">
+            <h2 className=" text-secondary-6 text-lg lg:text-xl xl:text-3xl">
+              <span className="text-accent-6"> Discover</span> Devotionals
+            </h2>
+            <button
+              className="mb-2 "
+              onClick={() => handleViewDevotion(devotions[0])}
+            >
+              <span className="border border-accent-6 hover:bg-accent-6 hover:text-primary-1 px-2 rounded-full text-accent-6 text-xs xl:px-3 xl:py-1 xl:mb-1 xl:text-sm">
+                All Devotionals
+              </span>
+            </button>
+          </div>
+          <motion.div
+            variants={gridContainerVariants}
+            initial="hidden"
+            animate="show"
+            className="grid grid-cols-2 justify-between mt-6 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4 mx-auto w-[90%]"
+          >
+            {devotions.slice(0, 4).map((devotion, index) => (
+              <motion.div
+                variants={gridSquareVariants}
+                whileHover={{
+                  scale: 1.06,
+                }}
+                whileTap={{ scale: 0.9 }}
+                transition={{
+                  bounceDamping: 10,
+                  bounceStiffness: 600,
+                }}
+                key={index}
+                className=" shadow-2xl rounded-2 overflow-hidden relative cursor-pointer"
+                onClick={() => handleViewDevotion(devotion)}
+              >
+                <img
+                  src={
+                    `https://ezra-seminary.mybese.tech/images/${devotion.image}` ||
+                    `${bible}`
+                  }
+                  alt={devotion.title}
+                  className="w-full h-auto object-contain rounded-lg"
+                />
+                <div className="absolute inset-0 h-full bg-accent-10 bg-opacity-60 rounded-lg">
+                  <ArrowSquareUpRight
+                    weight="fill"
+                    className="text-primary-1 text-lg md:text-xl lg:text-2xl xl:text-3xl absolute top-0 right-0 m-2"
+                  />
+                  <div className="absolute bottom-0 left-0 my-2 md:top-[65%] lg:top-[77%] xl:top-[79%]">
+                    <h3 className=" text-primary-1 text-sm xl:text-lg mx-2">
+                      {devotion.title}
+                    </h3>
+                    <p className="text-xs xl:text-sm mx-2 text-accent-2">
+                      {devotion.month} {devotion.day}
+                    </p>
+                  </div>
+                </div>
+              </motion.div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
     </div>
   );
 };
