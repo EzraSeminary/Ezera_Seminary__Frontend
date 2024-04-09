e16c4972a20c933a7458bb6258f951d225ef0894
Yidne <Yamiyekn@gmail.com>
2024-04-09 17:48:02 +0300

    Update the modal container and make it responsive for the mobile.

--- src/features/sabbathSchool/modal/Modal.tsx
+++ src/features/sabbathSchool/modal/Modal.tsx
@@ -6,21 +6,23 @@ const Modal = ({ showModal, toggleModal, children }: { showModal: boolean, toggl
 
   return (
     <div className="fixed inset-0 z-40 flex items-center justify-center">
-      <div className="fixed inset-0 bg-gray-500 bg-opacity-50" onClick={toggleModal} />
-      <div className="relative px-4 w-full max-w-2xl h-full md:h-auto">
-        <div className="modal-content relative bg-white rounded-lg shadow overflow-y-auto text-justify p-4" 
-             style={{ maxHeight: '50vh' }}>
+      <div
+        className="fixed inset-0 bg-gray-500 bg-opacity-50"
+        onClick={toggleModal}
+      />
+      <div className="relative px-4 w-full max-w-2xl h-auto ">
+        <div
+          className="modal-content relative bg-white rounded-lg shadow overflow-y-auto text-justify p-4"
+          style={{ maxHeight: "50vh" }}
+        >
           <div className="flex justify-end">
-            <X 
+            <X
               className="p-1 text-xl bg-accent-6 text-primary-1 rounded-full hover:bg-accent-8 hover:cursor-pointer transition-all"
               size={24}
               onClick={toggleModal}
-            >
-            </X>
+            ></X>
           </div>
-          <div className="py-2 -mt-8">
-            {children}
-          </div>
+          <div className="py-2 -mt-8">{children}</div>
         </div>
       </div>
     </div>
