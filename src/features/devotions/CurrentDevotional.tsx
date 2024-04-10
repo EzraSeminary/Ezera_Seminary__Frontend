bd845be990d5ba046bb8f2abc2c629ed50e6f170
Yidne <Yamiyekn@gmail.com>
2024-04-10 15:11:04 +0300

    Update prayer image position and size.

--- src/features/devotions/CurrentDevotional.tsx
+++ src/features/devotions/CurrentDevotional.tsx
@@ -270,8 +270,8 @@ const CurrentDevotional: React.FC<CurrentDevotionalProps> = ({
                 <p className=" font-nokia-bold text-1xl text-center px-8  py-2">
                   {devotionToDisplay.prayer}
                 </p>
-                <div className="absolute top-[30%] md:top-[20%] lg:top-[25%] xl:top-[20%] -left-8 bg-accent-6 rounded-full w-max  p-2">
-                  <img src="src/assets/prayerImg.png" alt="" className="" />
+                <div className="absolute top-[30%] md:top-[25%] lg:top-[30%] xl:top-[20%] -left-5 bg-accent-6 rounded-full w-max  p-2">
+                  <img src="src/assets/prayerImg.png" alt="" className="w-6" />
                 </div>
               </div>
             ) : (
@@ -279,7 +279,7 @@ const CurrentDevotional: React.FC<CurrentDevotionalProps> = ({
                 <p className="hidden font-nokia-bold text-1xl text-center border-2 border-accent-5 p-2 rounded text-accent-5">
                   {devotionToDisplay && devotionToDisplay.prayer}
                 </p>
-                <div className="absolute top-4 -left-8 bg-accent-6 rounded-full w-max  p-2">
+                <div className="absolute top-[30%] md:top-[25%] lg:top-[30%] xl:top-[20%] -left-5 bg-accent-6 rounded-full w-max  p-2">
                   <img src="src/assets/prayerImg.png" alt="" className="" />
                 </div>
               </div>
