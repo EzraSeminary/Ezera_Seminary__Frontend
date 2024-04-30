34ab5917c2e8c589fd179f81d8284afa878b2983
yeabtsega45 <yabtsega2022@gmail.com>
2024-04-30 17:15:56 +0300

    add element popup

--- src/features/courses/Elements/ElementData.ts
+++ src/features/courses/Elements/ElementData.ts
@@ -1,23 +1,20 @@
 export interface ElementData {
     title: string;
     description: string;
+    value:string;
   }
   
   export const elementData: ElementData[] = [
-    { title: "title", description: 'Description 1' },
-    { title: "sub", description: 'Description 2' },
-    { title: "text", description: 'Description 3' },
+    { title: "Title", description: 'Description 1' ,value:"title"},
+    { title: "Sub Title", description: 'Description 2' ,value:"sub"},
+    { title: "Paragraph", description: 'Description 3' ,value:"text"},
+    { title: "List", description: 'Description 1',value:"list" },
+    { title: "Slide", description: 'Description 1', value:"slide" },
+    { title: "Image", description: 'Description 2',value:"img" },
+    { title: "Quiz", description: 'Description 3',value:"quiz" },
+    { title: "Accordion", description: 'Description 2' ,value:"accordion"},
+    { title: "Sequence", description: 'Description 3' ,value:"sequence"},
+    { title: "Reveal", description: 'Description 1',value:"reveal" },
+    { title: "Range", description: 'Description 2' ,value:"range"},
+    { title: "Missing Words", description: 'Description 3' ,value:"dnd"},
   ];
-
-//   <option value=>Title</option>
-//           <option value=>Sub-title</option>
-//           <option value=>Paragraph</option>
-//           <option value="slide">Slide</option>
-//           <option value="img">Image</option>
-//           <option value="quiz">Quiz</option>
-//           <option value="list">List</option>
-//           <option value="accordion">Accordion</option>
-//           <option value="sequence">Sequence</option>
-//           <option value="reveal">Reveal</option>
-//           <option value="range">Range</option>
-//           <option value="dnd">Missing Words</option>
