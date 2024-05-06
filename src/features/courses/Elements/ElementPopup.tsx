e6beb0f1f79e93659e1cfdc5870aa8f91eaa135f
yeabtsega45 <yabtsega2022@gmail.com>
2024-05-06 14:12:21 +0300

    add element popup

--- src/features/courses/Elements/ElementPopup.tsx
+++ src/features/courses/Elements/ElementPopup.tsx
@@ -12,13 +12,16 @@ function ElementPopup({
 }: ElementPopupProps) {
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-gray-800 bg-opacity-50 z-50">
-      <div className="relative flex flex-col items-center justify-center bg-secondary-2 w-[90%] h-[90%] rounded-xl">
-        <XCircle
-          size={32}
-          onClick={closeElementPopup}
-          className="cursor-pointer absolute top-1 right-1"
-        />
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="flex flex-col items-center justify-center bg-secondary-2 w-[90%] h-[90%] rounded-2xl">
+        <div className="flex justify-between items-center w-full h-[10%] px-5 bg-white border border-secondary-3 rounded-t-2xl">
+          <h2 className="text-2xl">Slide Library</h2>
+          <XCircle
+            size={32}
+            onClick={closeElementPopup}
+            className="cursor-pointer"
+          />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 pt-4 w-[60%] h-full overflow-y-auto">
           {elementData.map((element: ElementData, index: number) => (
             <div
               key={index}
@@ -27,10 +30,10 @@ function ElementPopup({
                 onSelectElement(element.value);
                 closeElementPopup();
               }}
-              className="bg-white rounded-lg shadow-md p-4 hover:scale-105 transition-transform duration-300 ease-in-out hover:cursor-pointer"
+              className="bg-white rounded-lg shadow-md p-4 text-center hover:scale-105 transition-transform duration-300 ease-in-out hover:cursor-pointer border-2 hover:border-accent-6"
             >
               <h2 className="text-xl font-nokia-bold">{element.title}</h2>
-              <p className="text-secondary-7">{element.description}</p>
+              <p className="text-secondary-4">{element.description}</p>
             </div>
           ))}
         </div>
