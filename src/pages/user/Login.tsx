1180b73f65964c25ae5c233cf45b5595fb8be815
Besufekad-HAZ <besurye@gmail.com>
2024-05-23 16:21:29 +0300

    Import forgot password component inside login component

--- src/pages/user/Login.tsx
+++ src/pages/user/Login.tsx
@@ -10,6 +10,7 @@ import { useFormik } from "formik";
 import Footer from "@/components/Footer";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ForgotPassword from "@/components/ForgotPassword";
 
 interface APIError extends Error {
   status: number;
