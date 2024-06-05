d3e76cd63acd3e2da6b9a4e6e6fc74cb47c8032f
Besufekad-HAZ <besurye@gmail.com>
2024-06-05 12:39:56 +0300

    Remove unused phosphor icon

--- src/pages/user/Login.tsx
+++ src/pages/user/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { GoogleLogo, FacebookLogo } from "@phosphor-icons/react";
+import { GoogleLogo } from "@phosphor-icons/react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useLoginMutation } from "@/redux/api-slices/apiSlice";
