b0eca3846860f79ea7bdda5e942e170e13a9e6b9
yeabtsega45 <yabtsega2022@gmail.com>
2024-06-06 13:47:08 +0300

    change inputs to custom inputs

--- src/features/courses/admin/manage-course/EditElements.tsx
+++ src/features/courses/admin/manage-course/EditElements.tsx
@@ -456,6 +456,7 @@ function EditElements({ chapterIndex, slideIndex }: EditElementsProps) {
           onChange={handleSequenceInputChange}
           placeholder="Enter sequence item"
           className="border outline-accent-6 border-accent-5 bg-primary-4 text-secondary-6 rounded-md  font-bold px-2 py-1 w-full placeholder:text-sm placeholder:text-secondary-3"
+          maxLength={100}
         />
         <div className="flex justify-between items-center gap-2 mt-2 w-[80%] mx-auto">
           <button
@@ -587,6 +588,7 @@ function EditElements({ chapterIndex, slideIndex }: EditElementsProps) {
                 onChange={(e) => handleRevealTitleChange(index, e.target.value)}
                 placeholder={`Title ${index + 1}`}
                 className="mt-1  border outline-accent-6 border-accent-5 bg-primary-4 text-secondary-6 rounded-md  font-bold px-2 py-1 w-full placeholder:text-sm placeholder:text-secondary-3"
+                maxLength={50}
               />
               <CustomTextarea
                 value={revealContents[index]}
@@ -595,6 +597,7 @@ function EditElements({ chapterIndex, slideIndex }: EditElementsProps) {
                 }
                 placeholder={`Content ${index + 1}`}
                 className="mt-1  border outline-accent-6 border-accent-5 bg-primary-4 text-secondary-6 rounded-md  font-bold px-2 py-1 w-full placeholder:text-sm placeholder:text-secondary-3"
+                maxLength={200}
               />
               <Trash
                 onClick={() => {
@@ -666,6 +669,7 @@ function EditElements({ chapterIndex, slideIndex }: EditElementsProps) {
           onChange={handleDndQuestionChange}
           placeholder="Enter quiz question"
           className="border outline-accent-6 border-accent-5 bg-primary-4 text-secondary-6  rounded-md  font-bold px-2 py-1 w-full placeholder:text-sm placeholder:text-secondary-3"
+          maxLength={150}
         />
 
         <div className="flex justify-between items-center gap-2 mt-2 w-[80%] mx-auto">
@@ -705,6 +709,7 @@ function EditElements({ chapterIndex, slideIndex }: EditElementsProps) {
                 onChange={(e) => handleDndChoiceChange(index, e.target.value)}
                 placeholder={`Choice ${index + 1}`}
                 className="mt-1 border-2 border-accent-6 rounded-md text-accent-6 font-bold px-2 py-1 w-[75%]"
+                maxLength={50}
               />
               <Trash
                 onClick={() => {
