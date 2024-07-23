058102111489d1c6ac923310c2de613cf970d875
Besufekad-HAZ <besurye@gmail.com>
2024-07-23 18:33:14 +0300

    Add Redux Toolkit Slice for Video Links to handle the CRUD operations

---
+++ src/services/videoLinksApi.ts
@@ -0,0 +1,40 @@
+// videoLinksApi.js
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const videoLinksApi = createApi({
+  reducerPath: "videoLinksApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5100/" }), // Make sure this matches your backend URL and port
+  endpoints: (builder) => ({
+    getVideoLink: builder.query({
+      query: ({ year, quarter, lesson }) =>
+        `sslLinks/${year}/${quarter}/${lesson}`,
+    }),
+    addVideoLink: builder.mutation({
+      query: (newLink) => ({
+        url: "sslLinks",
+        method: "POST",
+        body: newLink,
+      }),
+    }),
+    updateVideoLink: builder.mutation({
+      query: ({ year, quarter, lesson, ...updatedLink }) => ({
+        url: `sslLinks/${year}/${quarter}/${lesson}`,
+        method: "PUT",
+        body: updatedLink,
+      }),
+    }),
+    deleteVideoLink: builder.mutation({
+      query: ({ year, quarter, lesson }) => ({
+        url: `sslLinks/${year}/${quarter}/${lesson}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetVideoLinkQuery,
+  useAddVideoLinkMutation,
+  useUpdateVideoLinkMutation,
+  useDeleteVideoLinkMutation,
+} = videoLinksApi;
