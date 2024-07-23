57df0b191271cd3810a0f7a72ff23f07cc9dbf0f
Besufekad-HAZ <besurye@gmail.com>
2024-07-23 18:43:04 +0300

    Remove Unnecessary declarations

--- src/features/sabbathSchool/ManageYoutubLink.tsx
+++ src/features/sabbathSchool/ManageYoutubLink.tsx
@@ -1,5 +1,5 @@
 // ManageYouTubeLink.js
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import {
   useAddVideoLinkMutation,
   useUpdateVideoLinkMutation,
