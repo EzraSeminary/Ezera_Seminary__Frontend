cb29ab84487164ab81db0c16d7f454f9d379e707
AmanWorku <amexworku2014@gmail.com>
2024-09-27 12:18:15 +0300

    Fix build error

--- src/pages/context/AuthContext.tsx
+++ src/pages/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import axios from 'axios';
 
 interface User {
