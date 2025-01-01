019f5cf092b7de2c0e85d6e5e7f56237441bb291
AmanWorku <amexworku2014@gmail.com>
2025-01-01 09:06:35 +0300

    Update publishing issue

--- src/features/courses/admin/manage-course/EditCourse.tsx
+++ src/features/courses/admin/manage-course/EditCourse.tsx
@@ -1,20 +1,20 @@
-import { FormEvent, useEffect, useState } from "react";
+import useAxiosInstance from "@/api/axiosInstance";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import BeatLoader from "react-spinners/BeatLoader";
-import useAxiosInstance from "@/api/axiosInstance";
+import { FormEvent, useEffect, useState } from "react";
 import {
   MixElement,
   selectCourse,
   setCourse,
   togglePublished,
 } from "@/redux/courseSlice";
-import { RootState } from "@/redux/store";
 import { ArrowCircleLeft, ArrowSquareOut, Pen } from "@phosphor-icons/react";
 import EditChapters from "./EditChapters";
 import EditCourseFirst from "./EditCourseFirst";
+import BeatLoader from "react-spinners/BeatLoader";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { RootState } from "@/redux/store";
 
 function EditCourse() {
   const { id } = useParams();
@@ -25,9 +25,9 @@ function EditCourse() {
   const basePath = role;
 
   const [loading, setLoading] = useState(true);
-  const [updateLoading, setUpdateLoading] = useState(false);
-  const [publishLoading, setPublishLoading] = useState(false);
-  const [progress, setProgress] = useState(0);
+  const [isPublishClicked, setIsPublishClicked] = useState(false);
+  const [isSaveLoading, setIsSaveLoading] = useState(false);
+  const [isPublishLoading, setIsPublishLoading] = useState(false);
   const [showComponent, setShowComponent] = useState(false);
   const [shouldConfirmUnload, setShouldConfirmUnload] = useState(true);
 
@@ -56,42 +56,68 @@ function EditCourse() {
   }, [id, dispatch]);
 
   useEffect(() => {
+    if (isPublishClicked) {
+      handleSubmit();
+      setIsPublishClicked(false);
+    }
+  }, [course.published, isPublishClicked]);
+
+  useEffect(() => {
     const handleBeforeUnload = (event: BeforeUnloadEvent) => {
-      if (shouldConfirmUnload) {
+      const isReload = performance.getEntriesByType("navigation").some((nav) => {
+        return (nav as PerformanceNavigationTiming).type === "reload";
+      });
+  
+      if (!isReload && shouldConfirmUnload) {
         event.preventDefault();
-        event.returnValue = ""; // This shows the confirmation dialog
+        event.returnValue = ""; // Show the confirmation dialog
       }
     };
-
+  
     window.addEventListener("beforeunload", handleBeforeUnload);
-
+  
     return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
     };
   }, [shouldConfirmUnload]);
+  
 
-  const handleUpdate = (event?: FormEvent) => {
+  const handleNavigation = (event: Event) => {
+    if (shouldConfirmUnload) {
+      const confirmationMessage = "You have unsaved changes, do you really want to leave?";
+      if (!window.confirm(confirmationMessage)) {
+        event.preventDefault();
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("beforeunload", handleNavigation);
+    return () => {
+      window.removeEventListener("beforeunload", handleNavigation);
+    };
+  }, [shouldConfirmUnload]);
+
+  const handleSubmit = (event?: FormEvent) => {
     event?.preventDefault();
-    setUpdateLoading(true);
-    setShouldConfirmUnload(false); // Disable confirmation dialog
+    setIsSaveLoading(true);
+    setShouldConfirmUnload(false);
 
     const formData = new FormData();
     formData.append("title", course.title);
     formData.append("description", course.description);
     formData.append("category", course.category);
-
     if (typeof course.image === "string") {
       formData.append("image", course.image);
     } else if (course.image instanceof File) {
       formData.append("image", course.image, course.image.name);
     }
-
     formData.append("chapters", JSON.stringify(course.chapters));
     formData.append("published", String(course.published));
 
     course.chapters.forEach((chapter, chapterIndex) => {
       chapter.slides.forEach((slide, slideIndex) => {
-        slide.elements.forEach((element, ) => {
+        slide.elements.forEach((element, elementIndex) => {
           if (element.type === "img" && element.value instanceof File) {
             formData.append(
               `chapter_${chapterIndex}_slide_${slideIndex}_img`,
@@ -116,6 +142,12 @@ function EditCourse() {
                 mixElement.value.file,
                 `${chapterIndex}_${slideIndex}_${mixElement.value.file.name}`
               );
+            } else {
+              console.error(
+                "File missing in Mix Element:",
+                elementIndex,
+                mixElement.value.file
+              );
             }
           }
         });
@@ -127,56 +159,37 @@ function EditCourse() {
         headers: {
           "Content-Type": "multipart/form-data",
         },
-        onUploadProgress: (progressEvent) => {
-          const percentage = Math.round(
-            progressEvent.total ? (progressEvent.loaded * 100) / progressEvent.total : 0
-          );
-          setProgress(percentage);
-        },
       })
-      .then(() => {
-        toast.success(`Course updated successfully!`);
+      .then((res) => {
+        console.log(res);
+        toast.success(`Updating "${course.title}"!`, {
+          onClose: () => {
+            setShouldConfirmUnload(false); // Disable confirmation dialog
+            setTimeout(() => {
+              window.location.reload();
+            });
+          },
+        });
       })
       .catch((err) => {
-        toast.error(`Error updating course: ${err.message}`);
+        toast.error(`Error updating course: "${err.message}". Please try again.`);
+        console.log(err);
       })
       .finally(() => {
-        setUpdateLoading(false);
-        setProgress(0);
-        setShouldConfirmUnload(true); // Re-enable confirmation dialog
-        window.location.reload();
+        setIsSaveLoading(false);
+        setShouldConfirmUnload(true);
       });
   };
 
+  const handleButtonClick = () => {
+    setShowComponent(true);
+  };
+
   const handlePublish = () => {
-    setPublishLoading(true);
-    setShouldConfirmUnload(false); // Disable confirmation dialog
+    setIsPublishLoading(true);
+    setShouldConfirmUnload(false);
     dispatch(togglePublished());
-
-    const formData = new FormData();
-    formData.append("published", String(!course.published));
-
-    instance
-      .put(`/course/update/${id}`, formData, {
-        onUploadProgress: (progressEvent) => {
-          const percentage = Math.round(
-            progressEvent.total ? (progressEvent.loaded * 100) / progressEvent.total : 0
-          );
-          setProgress(percentage);
-        },
-      })
-      .then(() => {
-        toast.success(`Course ${course.published ? "unpublished" : "published"} successfully!`);
-      })
-      .catch((err) => {
-        toast.error(`Error updating publish status: ${err.message}`);
-      })
-      .finally(() => {
-        setPublishLoading(false);
-        setProgress(0);
-        setShouldConfirmUnload(true); // Re-enable confirmation dialog
-        window.location.reload();
-      });
+    setIsPublishClicked(true);
   };
 
   if (loading)
@@ -197,16 +210,16 @@ function EditCourse() {
       <ToastContainer />
       <div className="w-full">
         <div className="flex justify-between bg-secondary-6 rounded-t-lg px-6 py-3">
-          <div className="flex items-center">
+          <div className="flex items-center ">
             <Link to={`/${basePath}/course/edit`} className="flex items-center">
               <ArrowCircleLeft
                 weight="fill"
                 size={32}
                 className="text-accent-6 hover:text-accent-7 transition-all"
               />
-            </Link>
+            </Link>{" "}
             <button
-              onClick={() => setShowComponent(true)}
+              onClick={handleButtonClick}
               className="ml-3 flex items-center bg-gray-200 rounded-3xl px-4 py-1 border hover:border-gray-400 transition-all"
             >
               <p className="text-accent-6 font-nokia-bold text-sm pr-4">
@@ -224,50 +237,45 @@ function EditCourse() {
               </p>
             )}
           </div>
-          <div className="flex items-center gap-2">
-            {progress > 0 && (
-              <span className="text-sm text-accent-6 font-nokia-bold mr-2">{progress}%</span>
-            )}
-            <button
-              onClick={handlePublish}
-              className={`w-max px-4 py-1 flex justify-center items-center gap-2 font-semibold text-accent-6 bg-primary-6 rounded-lg transition-all border border-accent-6 ${
-                publishLoading ? "opacity-50 cursor-not-allowed" : ""
-              }`}
-              disabled={publishLoading}
-            >
-              {publishLoading ? (
-                <BeatLoader size={8} color="#EA9215" />
-              ) : !course.published ? (
-                <span>Publish</span>
-              ) : (
-                <span>Unpublish</span>
-              )}
-              <ArrowSquareOut
-                size={22}
-                weight="fill"
-                className="self-centered text-accent-6"
-              />
-            </button>
-            <button
-              onClick={handleUpdate}
-              className={`w-max px-4 py-1 flex justify-center items-center gap-2 ml-1 font-semibold text-primary-6 bg-accent-6 rounded-lg hover:bg-accent-7 transition-all ${
-                updateLoading ? "opacity-50 cursor-not-allowed" : ""
-              }`}
-              disabled={updateLoading}
-            >
-              {updateLoading ? (
-                <BeatLoader size={8} color="#FFFFFF" />
-              ) : (
-                <span>Update</span>
-              )}
-              <ArrowSquareOut
-                size={22}
-                weight="fill"
-                className="self-centered"
-              />
-            </button>
-          </div>
+          <div className="flex">
+      <button
+        onClick={handlePublish}
+        className="w-max px-2 flex justify-center items-center gap-2 font-semibold text-accent-6 bg-primary-6 rounded-lg transition-all border border-accent-6"
+        disabled={isPublishLoading} // Disable publish button when loading
+      >
+        {isPublishLoading ? (
+          <BeatLoader size={8} color={"#EA9215"} />
+        ) : !course.published ? (
+          <span>Publish</span>
+        ) : (
+          <span>Unpublish</span>
+        )}
+        <ArrowSquareOut
+          size={22}
+          weight="fill"
+          className="self-centered text-accent-6"
+        />
+      </button>
+
+      <button
+        onClick={handleSubmit}
+        className="w-max px-2 flex justify-center items-center gap-2 ml-1 font-semibold text-primary-6 bg-accent-6 rounded-lg hover:bg-accent-7 transition-all"
+        disabled={isSaveLoading} // Disable save button when loading
+      >
+        {isSaveLoading ? (
+          <BeatLoader size={8} color={"#FFFFFF"} />
+        ) : (
+          <span>Update</span>
+        )}
+        <ArrowSquareOut
+          size={22}
+          weight="fill"
+          className="self-centered"
+        />
+      </button>
+    </div>
         </div>
+        {/* display the edit course or edit chapters */}
         {showComponent ? (
           <EditCourseFirst setShowComponent={setShowComponent} />
         ) : (
